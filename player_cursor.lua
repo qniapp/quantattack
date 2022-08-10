66216c9419ca9e01e2f68521716c737f66a92d31
Yasuhito Takamiya <yasuhito@gmail.com>
2022-08-10 12:05:01 +0900

    リファクタリング

--- player_cursor.lua
+++ player_cursor.lua
@@ -1,9 +1,9 @@
 player_cursor = {
-  _sprites = {
-    ["corner"] = 65,
-    ["middle"] = 66,
+  sprites = {
+    corner = 65,
+    middle = 66,
   },
-  _color = colors.dark_green,
+  color = colors.dark_green,
 }
 
 function player_cursor:new(x, y, board)
@@ -11,159 +11,142 @@ function player_cursor:new(x, y, board)
     x = x,
     y = y,
     board = board,
-    _tick = 0,
+    tick = 0,
     warn = false,
-    _sm = state_machine:new(),
+    state_machine = state_machine:new(),
 
-    move_left = player_cursor.move_left,
-    move_right = player_cursor.move_right,
-    move_up = player_cursor.move_up,
-    move_down = player_cursor.move_down,
+    move_left = function(self)
+      if self.x == 1 then
+        self.warn = true
+      else
+        self.x -= 1
+      end
+    end,
 
-    update = player_cursor.update,
-    draw = player_cursor.draw,
+    move_right = function(self)
+      if self.x == self.board.cols - 1 then
+        self.warn = true
+      else
+        self.x += 1
+      end
+    end,
 
-    is_idle = player_cursor.is_idle,
-    is_shrunk = player_cursor.is_shrunk,
+    move_up = function(self)
+      if self.y == 1 then
+        self.warn = true
+      else
+        self.y -= 1
+      end
+    end,
+
+    move_down = function(self)
+      if self.y == self.board.rows then
+        self.warn = true
+      else
+        self.y += 1
+      end
+    end,  
+
+    update = function(self)
+      self.state_machine:update(self)
+    end,
+
+    draw = function(self)
+      self.state_machine:draw(self)
+    end,
 
     -- private
 
-    _change_state = player_cursor._change_state,
+    _advance_tick = function(self)
+      assert(self.tick >= 0 and self.tick < 30)
+
+      self.tick += 1
+      if (self.tick == 30) self.tick = 0
+    end,
+
+    _draw_sprites = function(self, xl, xr, yt, yb)
+      if self.warn then
+        pal(player_cursor.color, colors.red)
+      end
+      if self.game_over then
+        pal(player_cursor.color, colors.dark_grey)
+      end
+
+      spr(player_cursor.sprites.corner, xl, yt)
+      spr(player_cursor.sprites.middle, self:_screen_xm(), yt)
+      spr(player_cursor.sprites.corner, xr, yt, 1, 1, true, false)
+      spr(player_cursor.sprites.corner, xl, yb, 1, 1, false, true)
+      spr(player_cursor.sprites.middle, self:_screen_xm(), yb, 1, 1, false, true)
+      spr(player_cursor.sprites.corner, xr, yb, 1, 1, true, true)
+
+      pal(player_cursor.color, player_cursor.color)
+    end,
+
+    _screen_xl = function(self)
+      return self.board:screen_x(self.x) - 5
+    end,
+
+    _screen_xm = function(self)
+      return self.board:screen_x(self.x) + quantum_gate.size - 4
+    end,
+
+    _screen_xr = function(self)
+      return self.board:screen_x(self.x + 1) + 3
+    end,
+
+    _screen_yt = function(self)
+      return self.board:screen_y(self.y) - 5
+    end,
+
+    _screen_yb = function(self)
+      return self.board:screen_y(self.y) + quantum_gate.size - 4
+    end
   }
 
-  c:_change_state("idle")
+  c.state_machine:add_state(
+    "idle",
+    -- transition function
+    function(pc)
+      if (pc.tick >= 15) return "shrunk"
+      return "idle"
+    end,
+  
+    -- update function
+    function(pc)
+      pc:_advance_tick()
+    end,
+
+    -- draw function
+    function(pc)
+      pc:_draw_sprites(pc:_screen_xl(),
+                       pc:_screen_xr(),
+                       pc:_screen_yt(),
+                       pc:_screen_yb())    
+    end    
+  )
+
+  c.state_machine:add_state(
+    "shrunk",
+    -- transition function
+    function(pc)
+      if (pc.tick <= 14) return "idle"
+      return "shrunk"    
+    end,
+  
+    -- update function
+    function(pc)
+      pc:_advance_tick()
+    end,
+
+    -- draw function
+    function(pc)
+      pc:_draw_sprites(pc:_screen_xl() + 1,
+                       pc:_screen_xr() - 1,
+                       pc:_screen_yt() + 1,
+                       pc:_screen_yb() - 1)       
+    end
+  )  
+  c.state_machine:set_state("idle")
 
   return c
-end
-
-function player_cursor:move_left()
-  if self.x == 1 then
-    self.warn = true
-  else
-    self.x -= 1
-  end
-end
-
-function player_cursor:move_right()
-  if self.x == self.board.cols - 1 then
-    self.warn = true
-  else
-    self.x += 1
-  end
-end
-
-function player_cursor:move_up()
-  if self.y == 1 then
-    self.warn = true
-  else
-    self.y -= 1
-  end
-end
-
-function player_cursor:move_down()
-  if self.y == self.board.rows then
-    self.warn = true
-  else
-    self.y += 1
-  end
-end
-
-function player_cursor:update()
-  assert(self._tick >= 0 and self._tick < 30)
-
-  -- tick == 0 ... 14: state -> "idle"
-  if self._tick == 15 then
-    self:_change_state("shrunk")
-  end
-
-  -- tick == 15 ... 29: state -> "shrunk"
-  if self._tick == 29 then
-    self:_change_state("idle")
-  end
-
-  self._tick += 1
-  if self._tick == 30 then
-    self._tick = 0
-  end
-end
-
-function player_cursor:draw()
-  local x_left = self.board:screen_x(self.x)
-  local x_right = self.board:screen_x(self.x + 1)
-  local y = self.board:screen_y(self.y)
-
-  -- top left
-  local xtl = x_left - 5
-  local ytl = y - 5
-
-  -- top right
-  local xtr = x_right + 4
-  local ytr = ytl
-
-  -- bottom left
-  local xbl = xtl
-  local ybl = y + quantum_gate.size - 4
-
-  -- bottom right
-  local xbr = xtr
-  local ybr = ybl
-
-  -- top middle
-  local xtm = x_left + quantum_gate.size - 4
-  local ytm = ytl
-
-  -- bottom middle
-  local xbm = xtm
-  local ybm = ybl
-
-  if self:is_shrunk() then
-    xtl += 1
-    ytl += 1
-    xtr -= 1
-    ytr += 1
-    xbl += 1
-    ybl -= 1
-    xbr -= 1
-    ybr -= 1
-    ytm += 1
-    ybm -= 1
-  end
-
-  if self.warn then
-    pal(player_cursor._color, colors.red)
-  end
-  if self.game_over then
-    pal(player_cursor._color, colors.dark_grey)
-  end
-
-  spr(player_cursor._sprites.corner, xtl, ytl)
-  spr(player_cursor._sprites.corner, xtr, ytr, 1, 1, true, false)
-  spr(player_cursor._sprites.corner, xbl, ybl, 1, 1, false, true)
-  spr(player_cursor._sprites.corner, xbr, ybr, 1, 1, true, true)
-  spr(player_cursor._sprites.middle, xtm, ytm)
-  spr(player_cursor._sprites.middle, xbm, ybm, 1, 1, false, true)
-
-  pal(player_cursor._color, player_cursor._color)
-end
-
-function player_cursor:is_idle()
-  return self._state == "idle"
-end
-
-function player_cursor:is_shrunk()
-  return self._state == "shrunk"
-end
-
-function player_cursor:_change_state(new_state)
-  assert(new_state)
-  assert(new_state == "idle" or new_state == "shrunk")
-
-  if new_state == "idle" then
-    assert(self._state == nil or self:is_shrunk())
-  end
-  if new_state == "shrunk" then
-    assert(self:is_idle())
-  end
-
-  self._state = new_state
 end
--- state_machine.lua
+++ state_machine.lua
@@ -7,24 +7,31 @@
 ------------------------------------------------------------------------
 state_machine = {}
 
+-- state-machines-du -------------------------------------------------------
+-- copyright (c) 2021 jason delaat
+-- mit license: https://github.com/jasondelaat/pico8-tools/blob/release/license
+----------------------------------------------------------------------------
+-- simple state machine manager with draw and update functions.
+-- token count: 107
+------------------------------------------------------------------------
+state_machine = {}
+
 function state_machine:new()
    return {
       _current_state=nil,
       add_state=state_machine.add_state,
       set_state=state_machine.set_state,
       get_state=state_machine.get_state,
-      update=state_machine.update
+      update=state_machine.update,
+      draw=state_machine.draw
    }
 end
 
-function state_machine:add_state(name, transition, setup, teardown)
-   self[name] = {transition=transition, setup=setup, teardown=teardown}
+function state_machine:add_state(name, transition, update, draw)
+   self[name] = {transition=transition, update=update, draw=draw}
 end
 
 function state_machine:set_state(name)
-   if self._current_state == nil then
-      self[name].setup()
-   end
    self._current_state = name
 end
 
@@ -32,12 +39,14 @@ function state_machine:get_state()
    return self[self._current_state]
 end
 
-function state_machine:update()
-   local state = self:get_state()
-   local new_state = state.transition()
+function state_machine:update(obj)
+   local new_state = self:get_state().transition(obj)
    if new_state != self._current_state then
-      state.teardown()
       self:set_state(new_state)
-      self:get_state().setup()
    end
+   self:get_state().update(obj)
+end
+
+function state_machine:draw(obj)
+   self:get_state().draw(obj)
 end
