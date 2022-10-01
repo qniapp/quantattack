1b83b94f28aecf44b7645375356802d5ae9d4743
Yasuhito Takamiya <yasuhito@gmail.com>
2022-10-02 08:22:34 +0900

    リファクタリング

--- src/board.lua
+++ src/board.lua
@@ -1,8 +1,9 @@
 require("engine/core/class")
+
 require("engine/quantum_gate_types")
 
 local quantum_gate = require("quantum_gate")
-local gate_reduction_rules = require("engine/gate_reduction_rules")
+local gate_reduction_rules = require("gate_reduction_rules")
 
 -- gate states
 
