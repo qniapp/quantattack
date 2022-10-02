e7739b9a110251742de40accf920c0cd1a0348a8
Yasuhito Takamiya <yasuhito@gmail.com>
2022-10-02 16:12:12 +0900

    リファクタリング

--- src/i_gate.lua
+++ src/i_gate.lua
@@ -1,7 +1,10 @@
-i_gate = {
-  new = function(self)
-    return quantum_gate("i")
-  end
-}
+require("engine/core/class")
+
+local quantum_gate = require("quantum_gate")
+local i_gate = derived_class(quantum_gate)
+
+function i_gate:_init()
+  quantum_gate._init(self, 'i')
+end
 
 return i_gate
