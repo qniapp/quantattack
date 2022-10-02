edafff14f2e765be66afc74d2fbe445aebb9ec23
Yasuhito Takamiya <yasuhito@gmail.com>
2022-10-02 16:26:52 +0900

    リファクタリング

--- src/control_gate.lua
+++ src/control_gate.lua
@@ -1,42 +1,41 @@
+require("engine/core/class")
+
 local quantum_gate = require("quantum_gate")
+local control_gate = derived_class(quantum_gate)
 
-control_gate = {
-  new = function(self, cnot_x_x)
-    assert(cnot_x_x)
+function control_gate:_init(cnot_x_x)
+  assert(cnot_x_x)
 
-    local c = quantum_gate("control")
-    c.cnot_x_x = cnot_x_x
-    c._sprites = {
-      idle = 6,
-      dropped = 22,
-      jumping = 54,
-      falling = 38,
-      match_up = 14,
-      match_middle = 30,
-      match_down = 46
-    }
+  quantum_gate._init(self, 'control')
+  self.cnot_x_x = cnot_x_x
+  self._sprites = {
+    idle = 6,
+    dropped = 22,
+    jumping = 54,
+    falling = 38,
+    match_up = 14,
+    match_middle = 30,
+    match_down = 46
+  }
+end
 
-    c.before_update = function(self)
-      if self.tick_connection == nil then
-        self.tick_connection = 0
-        if self.connection == nil then
-          self.connection = flr(rnd(2)) == 0
-        end
-        if self.connection then
-          self.connection_duration = flr(rnd(5)) * 30
-        else
-          self.connection_duration = flr(rnd(5)) + 5
-        end
-      elseif self.tick_connection == self.connection_duration then
-        self.tick_connection = nil
-        self.connection = not self.connection
-      else
-        self.tick_connection = self.tick_connection + 1
-      end
+function control_gate:before_update(self)
+  if self.tick_connection == nil then
+    self.tick_connection = 0
+    if self.connection == nil then
+      self.connection = flr(rnd(2)) == 0
     end
-
-    return c
+    if self.connection then
+      self.connection_duration = flr(rnd(5)) * 30
+    else
+      self.connection_duration = flr(rnd(5)) + 5
+    end
+  elseif self.tick_connection == self.connection_duration then
+    self.tick_connection = nil
+    self.connection = not self.connection
+  else
+    self.tick_connection = self.tick_connection + 1
   end
-}
+end
 
 return control_gate
--- src/gate_reduction_rules.lua
+++ src/gate_reduction_rules.lua
@@ -2,6 +2,7 @@ local x_gate = require("x_gate")
 local y_gate = require("y_gate")
 local z_gate = require("z_gate")
 local s_gate = require("s_gate")
+local control_gate = require("control_gate")
 local swap_gate = require("swap_gate")
 
 -- todo: merge with quantum_gate_types.lua
@@ -265,7 +266,7 @@ gate_reduction_rules = {
         type = "hh cnot hh",
         score = 800,
         to = { {}, { dx = dx },
-          { dy = 1, gate = cnot_x_gate:new(x + dx) }, { dx = dx, dy = 1, gate = control_gate:new(x) },
+          { dy = 1, gate = cnot_x_gate:new(x + dx) }, { dx = dx, dy = 1, gate = control_gate(x) },
           { dy = 2 }, { dx = dx, dy = 2 } },
       }
     end
