aa9bd99f46bd997e887d7a523f6d97001e52157a
Yasuhito Takamiya <yasuhito@gmail.com>
2022-10-06 08:22:14 +0900

    リファクタリング

--- src/quantum_gate_utest.lua
+++ src/quantum_gate_utest.lua
@@ -1,31 +1,36 @@
 require("engine/test/bustedhelper")
 
 local quantum_gate = require("quantum_gate")
+local h_gate = require("h_gate")
+local x_gate = require("x_gate")
 
 describe('quantum_gate', function()
   it('should update', function()
-    local gate = quantum_gate('h')
+    local gate = h_gate()
 
     assert.has_no.errors(function() gate:update() end)
   end)
 
   describe('render', function()
     it('should render without errors', function()
-      local gate = quantum_gate('h')
+      local gate = h_gate()
 
       assert.has_no.errors(function() gate:render(0, 0) end)
     end)
   end)
 
   describe('is_swappable', function()
-    it('should return true if the gate is idle', function()
-      local gate = quantum_gate('h')
+    local gate
 
+    before_each(function()
+      gate = h_gate()
+    end)
+
+    it('should return true if the gate is idle', function()
       assert.is_true(gate:is_swappable())
     end)
 
     it('should return true if the state is swap_finished', function()
-      local gate = quantum_gate('h')
       gate.state = 'swap_finished'
 
       assert.is_true(gate:is_swappable())
@@ -33,23 +38,23 @@ describe('quantum_gate', function()
   end)
 
   describe('swap_with_left', function()
-    it('should swap with the left gate without errors', function()
-      local gate = quantum_gate('h')
+    local gate
 
+    before_each(function()
+      gate = h_gate()
+    end)
+
+    it('should swap with the left gate without errors', function()
       assert.has_no.errors(function() gate:swap_with_left(1) end)
     end)
 
     it('should transition its state to swapping', function()
-      local gate = quantum_gate('h')
-
       gate:swap_with_left(1)
 
       assert.is_true(gate:is_swapping())
     end)
 
     it('should set new_x_after_swap', function()
-      local gate = quantum_gate('h')
-
       gate:swap_with_left(1)
 
       assert.are.equals(1, gate.new_x_after_swap)
@@ -57,23 +62,23 @@ describe('quantum_gate', function()
   end)
 
   describe('swap_with_right', function()
-    it('should swap with the right gate without errors', function()
-      local gate = quantum_gate('h')
+    local gate
 
+    before_each(function()
+      gate = h_gate()
+    end)
+
+    it('should swap with the right gate without errors', function()
       assert.has_no.errors(function() gate:swap_with_right(2) end)
     end)
 
     it('should transition its state to swapping', function()
-      local gate = quantum_gate('h')
-
       gate:swap_with_right(2)
 
       assert.is_true(gate:is_swapping())
     end)
 
     it('should set new_x_after_swap', function()
-      local gate = quantum_gate('h')
-
       gate:swap_with_right(2)
 
       assert.are.equals(2, gate.new_x_after_swap)
@@ -84,11 +89,17 @@ describe('quantum_gate', function()
     local gate
 
     before_each(function()
-      gate = quantum_gate('h')
+      gate = h_gate()
     end)
 
     it('should drop the gate without errors', function()
       assert.has_no.errors(function() gate:drop(1, 2) end)
+    end)
+
+    it('should transition its state to dropping', function()
+      gate:drop(1, 2)
+
+      assert.is_true(gate:is_dropping())
     end)
 
     it('should throw an error if start_y is out of range', function()
@@ -107,8 +118,8 @@ describe('quantum_gate', function()
   end)
 
   it('should replace with other gate', function()
-    local gate = quantum_gate('h')
-    local other_gate = quantum_gate('x')
+    local gate = h_gate()
+    local other_gate = x_gate()
 
     assert.has_no.errors(function() gate:replace_with(other_gate) end)
   end)
