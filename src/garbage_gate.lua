f375df157374f6042414c293071342f95c5f8dcf
Yasuhito Takamiya <yasuhito@gmail.com>
2022-10-06 17:03:26 +0900

    リファクタリング

--- src/garbage_gate.lua
+++ src/garbage_gate.lua
@@ -1,32 +1,30 @@
 require("engine/core/class")
 
 local quantum_gate = require("quantum_gate")
-local garbage_gate = derived_class(quantum_gate)
 
-function garbage_gate:_init(width, board)
-  assert(width ~= nil, "width is nil")
+local garbage_gate = derived_class(quantum_gate)
+garbage_gate._sprite = 57
+garbage_gate._sprite_left = 56
+garbage_gate._sprite_right = 58
+garbage_gate._ddy = 0.98
+
+function garbage_gate:_init(span, board)
+  assert(span ~= nil, "span is nil")
   assert(board ~= nil, "board is nil")
 
-  local random_x = flr(rnd(board.cols - width + 1)) + 1
-  local start_y = board:screen_y(1)
-  local stop_y = board:screen_y(board:gate_top_y(random_x, random_x + width - 1) - 1)
+  local random_x = flr(rnd(board.cols - span + 1)) + 1
+  local start_screen_y = board:screen_y(1)
+  local stop_screen_y = board:screen_y(board:gate_top_y(random_x, random_x + span - 1) - 1)
 
   quantum_gate._init(self, 'g')
-  self.width = width
+  self.span = span
   self._state = "fall"
   self.x = random_x
-  self.y = start_y
-  self.stop_y = stop_y
-
-  -- TODO: すべての garbage_gate インスタンスに共通するプロパティは
-  -- garbage_gate.foo として定義する
-  self._spr = 57
-  self._spr_left = 56
-  self._spr_right = 58
-  self._gate_top_y = stop_y + quantum_gate.size
-  self._sink_y = stop_y + quantum_gate.size * 2
+  self.y = start_screen_y
+  self.stop_screen_y = stop_screen_y
+  self._gate_top_y = stop_screen_y + quantum_gate.size
+  self._sink_y = stop_screen_y + quantum_gate.size * 2
   self._dy = 16
-  self._ddy = 0.98
 end
 
 function garbage_gate:update()
@@ -43,7 +41,7 @@ end
 function garbage_gate:_update_state()
   if self._state ~= "bounce" then
     if self._dy < 0.1 then
-      self:_change_state("bounce")
+      self._state = "bounce"
       self._dy = -7
     end
 
@@ -54,44 +52,39 @@ function garbage_gate:_update_state()
       self._dy = self._dy * 0.2
 
       if (self._state == "fall") then
-        self:_change_state("hit gate")
+        self._state = "hit gate"
         sfx(1)
       else
-        self:_change_state("sink")
+        self._state = "sink"
       end
     end
   else
     -- bounce
-    if self.y > self.stop_y and self._dy > 0 then
-      self.y = self.stop_y
+    if self.y > self.stop_screen_y and self._dy > 0 then
+      self.y = self.stop_screen_y
       self._dy = -self._dy * 0.6
     end
   end
 
-  if (self.y == self.stop_y and
+  if (self.y == self.stop_screen_y and
       self.y == self.y_prev) then
-    self:_change_state("idle")
+    self._state = "idle"
   end
 end
 
 function garbage_gate:_update_dy()
-  if (self._state ~= "bounce") then
+  if self._state ~= "bounce" then
     return
   end
-  self._dy = self._dy + self._ddy
+  self._dy = self._dy + garbage_gate._ddy
 end
 
 function garbage_gate:effect_dy()
   if self._state == "sink" or self._state == "bounce" then
-    return self.y - self.stop_y
+    return self.y - self.stop_screen_y
   else
     return 0
   end
-end
-
--- いらないので self._state = new_state とベタに書く
-function garbage_gate:_change_state(new_state)
-  self._state = new_state
 end
 
 -- TODO: state == "drop" との違いは？
@@ -101,13 +94,13 @@ function garbage_gate:is_fall()
 end
 
 function garbage_gate:render(screen_x, screen_y)
-  for x = 0, self.width - 1 do
-    local spr_id = self._spr
+  for x = 0, self.span - 1 do
+    local spr_id = garbage_gate._sprite
     if (x == 0) then
-      spr_id = self._spr_left
+      spr_id = garbage_gate._sprite_left
     end
-    if (x == self.width - 1) then
-      spr_id = self._spr_right
+    if (x == self.span - 1) then
+      spr_id = garbage_gate._sprite_right
     end
 
     if screen_y then
