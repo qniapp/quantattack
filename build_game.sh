a5521b4636156db931987555a4bb67c5c956c91a
Yasuhito Takamiya <yasuhito@gmail.com>
2022-10-06 16:38:12 +0900

    build_game_min.sh を追加

--- build_game.sh
+++ build_game.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./scripts/build_cartridge.sh src main.lua src -o build/game --data data.p8 -s assert,log,visual_logger,tuner,profiler,mouse -m 1
+./scripts/build_cartridge.sh src main.lua src -o build/game --data data.p8 -s assert -m 1
