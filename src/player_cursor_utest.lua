8671c155fba2b2b9098d6257dfbdd44692ab978f
Yasuhito Takamiya <yasuhito@gmail.com>
2022-10-09 09:20:55 +0900

    テストを追加

--- src/player_cursor_utest.lua
+++ src/player_cursor_utest.lua
@@ -24,6 +24,16 @@ describe('player_cursor', function()
       assert.are_equal(2, player_cursor.x)
       assert.are_equal(6, player_cursor.y)
     end)
+
+    it("should not move any further to the left when reaching the left edge", function()
+      player_cursor:move_left() -- x = 2
+      player_cursor:move_left() -- x = 1
+
+      player_cursor:move_left()
+
+      assert.are_equal(1, player_cursor.x)
+      assert.are_equal(6, player_cursor.y)
+    end)
   end)
 
   describe('move_right', function()
@@ -31,6 +41,16 @@ describe('player_cursor', function()
       player_cursor:move_right()
 
       assert.are_equal(4, player_cursor.x)
+      assert.are_equal(6, player_cursor.y)
+    end)
+
+    it("should not move any further to the right when reaching the right edge", function()
+      player_cursor:move_right() -- x = 4
+      player_cursor:move_right() -- x = 5
+
+      player_cursor:move_right()
+
+      assert.are_equal(5, player_cursor.x)
       assert.are_equal(6, player_cursor.y)
     end)
   end)
@@ -42,6 +62,19 @@ describe('player_cursor', function()
       assert.are_equal(3, player_cursor.x)
       assert.are_equal(5, player_cursor.y)
     end)
+
+    it("should not move any upper when reaching the top edge", function()
+      player_cursor:move_up() -- y = 5
+      player_cursor:move_up() -- y = 4
+      player_cursor:move_up() -- y = 3
+      player_cursor:move_up() -- y = 2
+      player_cursor:move_up() -- y = 1
+
+      player_cursor:move_up()
+
+      assert.are_equal(3, player_cursor.x)
+      assert.are_equal(1, player_cursor.y)
+    end)
   end)
 
   describe('move_down', function()
@@ -50,6 +83,20 @@ describe('player_cursor', function()
 
       assert.are_equal(3, player_cursor.x)
       assert.are_equal(7, player_cursor.y)
+    end)
+
+    it("should not move any further down when reaching the bottom", function()
+      player_cursor:move_down() -- y = 7
+      player_cursor:move_down() -- y = 8
+      player_cursor:move_down() -- y = 9
+      player_cursor:move_down() -- y = 10
+      player_cursor:move_down() -- y = 11
+      player_cursor:move_down() -- y = 12
+
+      player_cursor:move_down()
+
+      assert.are_equal(3, player_cursor.x)
+      assert.are_equal(12, player_cursor.y)
     end)
   end)
 
