d0dc0eb4926afe821cb666accc43cd1a68396659
Yasuhito Takamiya <yasuhito@gmail.com>
2022-10-16 10:23:45 +0900

    swap をはさんだゲートが消えない問題を修正 (#24)

--- i_gate.lua
+++
@@ -1,6 +0,0 @@
-i_gate = {
-  new = function(self)
-    return quantum_gate:new("i")
-  end
-}
-
--- src/garbage_gate.lua
+++ src/garbage_gate.lua
@@ -1,18 +1,14 @@
-require("engine/core/class")
-
 local gate = require("gate")
 
-local garbage_gate = derived_class(gate)
-
-function garbage_gate:_init(span)
+function garbage_gate(span)
   --#if assert
   assert(span ~= nil, "span is nil")
   --#endif
 
-  gate._init(self, 'g', span)
-  self._sprite_middle = 83
-  self._sprite_left = 82
-  self._sprite_right = 84
-end
+  local garbage = gate('g', span)
+  garbage._sprite_middle = 83
+  garbage._sprite_left = 82
+  garbage._sprite_right = 84
 
-return garbage_gate
+  return garbage
+end
