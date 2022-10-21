e99573aa83435b4925268e84dac89f7b9ec34400
Yasuhito Takamiya <yasuhito@gmail.com>
2022-10-21 11:16:16 +0900

    簡約ルールを追加 (#32)

--- src/board_utest.lua
+++ src/board_utest.lua
@@ -678,6 +678,322 @@ describe('board', function()
       assert.is_true(board:gate_at(3, 12)._reduce_to:is_i())
     end)
 
+    --  X            Y
+    --  S-S  ----->  S-S
+    --    Z            I
+    it('should reduce X S-S Z', function()
+      board:put(1, 10, x_gate())
+      board:put(1, 11, swap_gate(3))
+      board:put(3, 11, swap_gate(1))
+      board:put(3, 12, z_gate())
+
+      board:reduce_gates()
+
+      assert.are_equal("y", board:gate_at(1, 10)._reduce_to.type)
+      assert.are_equal("swap", board:gate_at(1, 11).type)
+      assert.are_equal("swap", board:gate_at(3, 11).type)
+      assert.is_true(board:gate_at(3, 12)._reduce_to:is_i())
+    end)
+
+    --  H            I
+    --  X            Z
+    --  S-S  ----->  S-S
+    --    H            I
+    it('should reduce HX S-S H', function()
+      board:put(1, 9, h_gate())
+      board:put(1, 10, x_gate())
+      board:put(1, 11, swap_gate(3))
+      board:put(3, 11, swap_gate(1))
+      board:put(3, 12, h_gate())
+
+      board:reduce_gates()
+
+      assert.is_true(board:gate_at(1, 9)._reduce_to:is_i())
+      assert.are_equal("z", board:gate_at(1, 10)._reduce_to.type)
+      assert.are_equal("swap", board:gate_at(1, 11).type)
+      assert.are_equal("swap", board:gate_at(3, 11).type)
+      assert.is_true(board:gate_at(3, 12)._reduce_to:is_i())
+    end)
+
+    --  H            Z
+    --  S-S  ----->  S-S
+    --    X            I
+    --    H            I
+    it('should reduce H S-S XH', function()
+      board:put(1, 9, h_gate())
+      board:put(1, 10, swap_gate(3))
+      board:put(3, 10, swap_gate(1))
+      board:put(3, 11, x_gate())
+      board:put(3, 12, h_gate())
+
+      board:reduce_gates()
+
+      assert.are_equal("z", board:gate_at(1, 9)._reduce_to.type)
+      assert.are_equal("swap", board:gate_at(1, 10).type)
+      assert.are_equal("swap", board:gate_at(3, 10).type)
+      assert.is_true(board:gate_at(3, 11)._reduce_to:is_i())
+      assert.is_true(board:gate_at(3, 12)._reduce_to:is_i())
+    end)
+
+    --  H            I
+    --  Z            X
+    --  S-S  ----->  S-S
+    --    H            I
+    it('should reduce HZ S-S H', function()
+      board:put(1, 9, h_gate())
+      board:put(1, 10, z_gate())
+      board:put(1, 11, swap_gate(3))
+      board:put(3, 11, swap_gate(1))
+      board:put(3, 12, h_gate())
+
+      board:reduce_gates()
+
+      assert.is_true(board:gate_at(1, 9)._reduce_to:is_i())
+      assert.are_equal("x", board:gate_at(1, 10)._reduce_to.type)
+      assert.are_equal("swap", board:gate_at(1, 11).type)
+      assert.are_equal("swap", board:gate_at(3, 11).type)
+      assert.is_true(board:gate_at(3, 12)._reduce_to:is_i())
+    end)
+
+    --  H            X
+    --  S-S  ----->  S-S
+    --    Z            I
+    --    H            I
+    it('should reduce H S-S ZH', function()
+      board:put(1, 9, h_gate())
+      board:put(1, 10, swap_gate(3))
+      board:put(3, 10, swap_gate(1))
+      board:put(3, 11, z_gate())
+      board:put(3, 12, h_gate())
+
+      board:reduce_gates()
+
+      assert.are_equal("x", board:gate_at(1, 9)._reduce_to.type)
+      assert.are_equal("swap", board:gate_at(1, 10).type)
+      assert.are_equal("swap", board:gate_at(3, 10).type)
+      assert.is_true(board:gate_at(3, 11)._reduce_to:is_i())
+      assert.is_true(board:gate_at(3, 12)._reduce_to:is_i())
+    end)
+
+    --  S            I
+    --  Z            Z
+    --  S-S  ----->  S-S
+    --    S            I
+    it('should reduce SZ S-S S', function()
+      board:put(1, 9, s_gate())
+      board:put(1, 10, z_gate())
+      board:put(1, 11, swap_gate(3))
+      board:put(3, 11, swap_gate(1))
+      board:put(3, 12, s_gate())
+
+      board:reduce_gates()
+
+      assert.is_true(board:gate_at(1, 9)._reduce_to:is_i())
+      assert.are_equal("z", board:gate_at(1, 10)._reduce_to.type)
+      assert.are_equal("swap", board:gate_at(1, 11).type)
+      assert.are_equal("swap", board:gate_at(3, 11).type)
+      assert.is_true(board:gate_at(3, 12)._reduce_to:is_i())
+    end)
+
+    --  S            Z
+    --  S-S  ----->  S-S
+    --    Z            I
+    --    S            I
+    it('should reduce S S-S ZS', function()
+      board:put(1, 9, s_gate())
+      board:put(1, 10, swap_gate(3))
+      board:put(3, 10, swap_gate(1))
+      board:put(3, 11, z_gate())
+      board:put(3, 12, s_gate())
+
+      board:reduce_gates()
+
+      assert.are_equal("z", board:gate_at(1, 9)._reduce_to.type)
+      assert.are_equal("swap", board:gate_at(1, 10).type)
+      assert.are_equal("swap", board:gate_at(3, 10).type)
+      assert.is_true(board:gate_at(3, 11)._reduce_to:is_i())
+      assert.is_true(board:gate_at(3, 12)._reduce_to:is_i())
+    end)
+
+    --  T            I
+    --  S            Z
+    --  S-S  ----->  S-S
+    --    T            I
+    it('should reduce TS S-S T', function()
+      board:put(1, 9, t_gate())
+      board:put(1, 10, s_gate())
+      board:put(1, 11, swap_gate(3))
+      board:put(3, 11, swap_gate(1))
+      board:put(3, 12, t_gate())
+
+      board:reduce_gates()
+
+      assert.is_true(board:gate_at(1, 9)._reduce_to:is_i())
+      assert.are_equal("z", board:gate_at(1, 10)._reduce_to.type)
+      assert.are_equal("swap", board:gate_at(1, 11).type)
+      assert.are_equal("swap", board:gate_at(3, 11).type)
+      assert.is_true(board:gate_at(3, 12)._reduce_to:is_i())
+    end)
+
+    --  T            Z
+    --  S-S  ----->  S-S
+    --    S            I
+    --    T            I
+    it('should reduce T S-S ST', function()
+      board:put(1, 9, t_gate())
+      board:put(1, 10, swap_gate(3))
+      board:put(3, 10, swap_gate(1))
+      board:put(3, 11, s_gate())
+      board:put(3, 12, t_gate())
+
+      board:reduce_gates()
+
+      assert.are_equal("z", board:gate_at(1, 9)._reduce_to.type)
+      assert.are_equal("swap", board:gate_at(1, 10).type)
+      assert.are_equal("swap", board:gate_at(3, 10).type)
+      assert.is_true(board:gate_at(3, 11)._reduce_to:is_i())
+      assert.is_true(board:gate_at(3, 12)._reduce_to:is_i())
+    end)
+
+    --  T            I
+    --  S-S  ----->  S-S
+    --    Z            I
+    --    S            I
+    --    T            I
+    it('should reduce T S-S ZST', function()
+      board:put(1, 8, t_gate())
+      board:put(1, 9, swap_gate(3))
+      board:put(3, 9, swap_gate(1))
+      board:put(3, 10, z_gate())
+      board:put(3, 11, s_gate())
+      board:put(3, 12, t_gate())
+
+      board:reduce_gates()
+
+      assert.is_true(board:gate_at(1, 8)._reduce_to:is_i())
+      assert.are_equal("swap", board:gate_at(1, 9).type)
+      assert.are_equal("swap", board:gate_at(3, 9).type)
+      assert.is_true(board:gate_at(3, 10)._reduce_to:is_i())
+      assert.is_true(board:gate_at(3, 11)._reduce_to:is_i())
+      assert.is_true(board:gate_at(3, 12)._reduce_to:is_i())
+    end)
+
+    --  T            I
+    --  S-S  ----->  S-S
+    --    S            I
+    --    Z            I
+    --    T            I
+    it('should reduce T S-S SZT', function()
+      board:put(1, 8, t_gate())
+      board:put(1, 9, swap_gate(3))
+      board:put(3, 9, swap_gate(1))
+      board:put(3, 10, s_gate())
+      board:put(3, 11, z_gate())
+      board:put(3, 12, t_gate())
+
+      board:reduce_gates()
+
+      assert.is_true(board:gate_at(1, 8)._reduce_to:is_i())
+      assert.are_equal("swap", board:gate_at(1, 9).type)
+      assert.are_equal("swap", board:gate_at(3, 9).type)
+      assert.is_true(board:gate_at(3, 10)._reduce_to:is_i())
+      assert.is_true(board:gate_at(3, 11)._reduce_to:is_i())
+      assert.is_true(board:gate_at(3, 12)._reduce_to:is_i())
+    end)
+
+    --  Z            I
+    --  H X          H I
+    --  X-C  ----->  X-C
+    --  H X          H I
+    it('should reduce Z HX X-C HX', function()
+      board:put(1, 9, z_gate())
+      board:put(1, 10, h_gate())
+      board:put(3, 10, x_gate())
+      board:put(1, 11, cnot_x_gate(3))
+      board:put(3, 11, control_gate(1))
+      board:put(1, 12, h_gate())
+      board:put(3, 12, x_gate())
+
+      board:reduce_gates()
+
+      assert.is_true(board:gate_at(1, 9)._reduce_to:is_i())
+      assert.are_equal("h", board:gate_at(1, 10).type)
+      assert.is_true(board:gate_at(3, 10)._reduce_to:is_i())
+      assert.are_equal("cnot_x", board:gate_at(1, 11).type)
+      assert.are_equal("control", board:gate_at(3, 11).type)
+      assert.are_equal("h", board:gate_at(1, 12).type)
+      assert.is_true(board:gate_at(3, 12)._reduce_to:is_i())
+    end)
+
+    --  X            I
+    --  H Z          H I
+    --  X-C  ----->  X-C
+    --  H            H
+    --  X            I
+    it('should reduce X HZ X-C H X', function()
+      board:put(1, 8, x_gate())
+      board:put(1, 9, h_gate())
+      board:put(3, 9, z_gate())
+      board:put(1, 10, cnot_x_gate(3))
+      board:put(3, 10, control_gate(1))
+      board:put(1, 11, h_gate())
+      board:put(1, 12, x_gate())
+
+      board:reduce_gates()
+
+      assert.is_true(board:gate_at(1, 8)._reduce_to:is_i())
+      assert.are_equal("h", board:gate_at(1, 9).type)
+      assert.is_true(board:gate_at(3, 9)._reduce_to:is_i())
+      assert.are_equal("cnot_x", board:gate_at(1, 10).type)
+      assert.are_equal("control", board:gate_at(3, 10).type)
+      assert.are_equal("h", board:gate_at(1, 11).type)
+      assert.is_true(board:gate_at(1, 12)._reduce_to:is_i())
+    end)
+
+    --  H Z          H I
+    --  X-C  ----->  X-C
+    --  H Z          H I
+    it('should reduce HZ X-C HZ', function()
+      board:put(1, 10, h_gate())
+      board:put(3, 10, z_gate())
+      board:put(1, 11, cnot_x_gate(3))
+      board:put(3, 11, control_gate(1))
+      board:put(1, 12, h_gate())
+      board:put(3, 12, z_gate())
+
+      board:reduce_gates()
+
+      assert.are_equal("h", board:gate_at(1, 10).type)
+      assert.is_true(board:gate_at(3, 10)._reduce_to:is_i())
+      assert.are_equal("cnot_x", board:gate_at(1, 11).type)
+      assert.are_equal("control", board:gate_at(3, 11).type)
+      assert.are_equal("h", board:gate_at(1, 12).type)
+      assert.is_true(board:gate_at(3, 12)._reduce_to:is_i())
+    end)
+
+    --  Z            I
+    --  H            H
+    --  X-C  ----->  X-C
+    --  H            H
+    --  Z            I
+    it('should reduce Z H X-C H Z', function()
+      board:put(1, 8, z_gate())
+      board:put(1, 9, h_gate())
+      board:put(1, 10, cnot_x_gate(3))
+      board:put(3, 10, control_gate(1))
+      board:put(1, 11, h_gate())
+      board:put(1, 12, z_gate())
+
+      board:reduce_gates()
+
+      assert.is_true(board:gate_at(1, 8)._reduce_to:is_i())
+      assert.are_equal("h", board:gate_at(1, 9).type)
+      assert.are_equal("cnot_x", board:gate_at(1, 10).type)
+      assert.are_equal("control", board:gate_at(3, 10).type)
+      assert.are_equal("h", board:gate_at(1, 11).type)
+      assert.is_true(board:gate_at(1, 12)._reduce_to:is_i())
+    end)
+
     --  C-X          I I
     --  S-S  ----->  S-S
     --  X-C          I I
--- src/reduction_rules.lua
+++ src/reduction_rules.lua
@@ -2,31 +2,17 @@ local reduction_rules = {
   h = {
     -- H          I
     -- H  ----->  I
-    {
-      "h\nh",
-      ",,\n,1,",
-      2
-    },
+    "h\nh|,,\n,1,|2|",
 
     -- H          I
     -- X          I
     -- H  ----->  Z
-    {
-      "h\nx\nh",
-      ",,\n,1,\n,2,z",
-      2,
-      3
-    },
+    "h\nx\nh|,,\n,1,\n,2,z|2|3",
 
     -- H          I
     -- Z          I
     -- H  ----->  X
-    {
-      "h\nz\nh",
-      ",,\n,1,\n,2,x",
-      3,
-      3
-    },
+    "h\nz\nh|,,\n,1,\n,2,x|3|3",
 
     -- H H          I I
     -- C-X  ----->  X-C
@@ -35,12 +21,7 @@ local reduction_rules = {
     -- H H          I I
     -- X-C  ----->  C-X
     -- H H          I I
-    {
-      "h,h\ncontrol,cnot_x\nh,h",
-      ",,\ntrue,,\n,1,cnot_x\ntrue,1,control\n,2,\ntrue,2,",
-      6,
-      10
-    },
+    "h,h\ncontrol,cnot_x\nh,h|,,\ntrue,,\n,1,cnot_x\ntrue,1,control\n,2,\ntrue,2,|6|10",
 
     -- H            I
     -- S-S  ----->  S-S
@@ -49,31 +30,70 @@ local reduction_rules = {
     --   H            I
     -- S-S  ----->  S-S
     -- H            I
-    {
-      "h\nswap,swap\n?,h",
-      ",,\ntrue,2,",
-      2,
-      5
-    }
+    "h\nswap,swap\n?,h|,,\ntrue,2,|2|5",
+
+    -- H            I
+    -- X            Z
+    -- S-S  ----->  S-S
+    --   H            I
+    --
+    --   H            I
+    --   X            Z
+    -- S-S  ----->  S-S
+    -- H            I
+    "h\nx\nswap,swap\n?,h|,,\n,1,z\ntrue,3,|3|7",
+
+    -- H            Z
+    -- S-S  ----->  S-S
+    --   X            I
+    --   H            I
+    --
+    --   H            Z
+    -- S-S  ----->  S-S
+    -- X            I
+    -- H            I
+    "h\nswap,swap\n?,x\n?,h|,,z\ntrue,2,\ntrue,3,|3|7",
+
+    -- H            I
+    -- Z            X
+    -- S-S  ----->  S-S
+    --   H            I
+    --
+    --   H            I
+    --   Z            X
+    -- S-S  ----->  S-S
+    -- H            I
+    "h\nz\nswap,swap\n?,h|,,\n,1,x\ntrue,3,|3|7",
+
+    -- H            X
+    -- S-S  ----->  S-S
+    --   Z            I
+    --   H            I
+    --
+    --   H            X
+    -- S-S  ----->  S-S
+    -- Z            I
+    -- H            I
+    "h\nswap,swap\n?,z\n?,h|,,x\ntrue,2,\ntrue,3,|3|7",
+
+    -- H Z          H I
+    -- X-C  ----->  X-C
+    -- H Z          H I
+    --
+    -- Z H          I H
+    -- C-X  ----->  X-C
+    -- Z H          I H
+    "h,z\ncnot_x,control\nh,z|true,,\ntrue,2,|2|10"
   },
 
   x = {
     -- X          I
     -- X  ----->  I
-    {
-      "x\nx",
-      ",,\n,1,",
-      2
-    },
+    "x\nx|,,\n,1,|2|",
 
     -- X          I
     -- Z  ----->  Y
-    {
-      "x\nz",
-      ",,\n,1,y",
-      2,
-      2
-    },
+    "x\nz|,,\n,1,y|2|2",
 
     -- X X          I I
     -- C-X  ----->  C-X
@@ -82,12 +102,7 @@ local reduction_rules = {
     -- X X          I I
     -- X-C  ----->  X-C
     --   X            I
-    {
-      "x,x\ncontrol,cnot_x\nx",
-      ",,\ntrue,,\n,2,",
-      3,
-      8
-    },
+    "x,x\ncontrol,cnot_x\nx|,,\ntrue,,\n,2,|3|8",
 
     -- X            I
     -- X-C  ----->  X-C
@@ -96,12 +111,7 @@ local reduction_rules = {
     --   X            I
     -- C-X  ----->  C-X
     --   X            I
-    {
-      "x\ncnot_x,control\nx",
-      ",,\n,2,",
-      2,
-      6
-    },
+    "x\ncnot_x,control\nx|,,\n,2,|2|6",
 
     -- X            I
     -- S-S  ----->  S-S
@@ -110,22 +120,35 @@ local reduction_rules = {
     --   X            I
     -- S-S  ----->  S-S
     -- X            I
-    {
-      "x\nswap,swap\n?,x",
-      ",,\ntrue,2,",
-      2,
-      5
-    }
+    "x\nswap,swap\n?,x|,,\ntrue,2,|2|5",
+
+    -- X            Y
+    -- S-S  ----->  S-S
+    --   Z            I
+    --
+    --   X            Y
+    -- S-S  ----->  S-S
+    -- T            I
+    "x\nswap,swap\n?,z|,,y\ntrue,2,|2|6",
+
+    -- X            I
+    -- H Z          H I
+    -- X-C  ----->  X-C
+    -- H            H
+    -- X            I
+    --
+    --   X            I
+    -- Z H          I H
+    -- C-X  ----->  C-X
+    --   H            H
+    --   X            I
+    "x\nh,z\ncnot_x,control\nh\nx|,,\ntrue,1,\n,4,|3|10",
   },
 
   y = {
     -- Y          I
     -- Y  ----->  I
-    {
-      "y\ny",
-      ",,\n,1,",
-      2
-    },
+    "y\ny|,,\n,1,|2|",
 
     -- Y            I
     -- S-S  ----->  S-S
@@ -134,31 +157,17 @@ local reduction_rules = {
     --   Y            I
     -- S-S  ----->  S-S
     -- Y            I
-    {
-      "y\nswap,swap\n?,y",
-      ",,\ntrue,2,",
-      2,
-      5
-    }
+    "y\nswap,swap\n?,y|,,\ntrue,2,|2|5",
   },
 
   z = {
     -- Z          I
     -- Z  ----->  I
-    {
-      "z\nz",
-      ",,\n,1,",
-      2
-    },
+    "z\nz|,,\n,1,|2|",
 
     -- Z          I
     -- X  ----->  Y
-    {
-      "z\nx",
-      ",,\n,1,y",
-      2,
-      2
-    },
+    "z\nx|,,\n,1,y|2|2",
 
     -- Z Z          I I
     -- C-X  ----->  C-X
@@ -167,12 +176,7 @@ local reduction_rules = {
     -- Z Z          I I
     -- X-C  ----->  X-C
     -- Z            I
-    {
-      "z,z\ncontrol,cnot_x\n?,z",
-      ",,\ntrue,,\ntrue,2,",
-      3,
-      8
-    },
+    "z,z\ncontrol,cnot_x\n?,z|,,\ntrue,,\ntrue,2,|3|8",
 
     -- Z            I
     -- C-X  ----->  C-X
@@ -181,12 +185,7 @@ local reduction_rules = {
     --   Z            I
     -- X-C  ----->  X-C
     --   Z            I
-    {
-      "z\ncontrol,cnot_x\nz",
-      ",,\n,2,",
-      2,
-      6
-    },
+    "z\ncontrol,cnot_x\nz|,,\n,2,|2|6",
 
     -- Z            I
     -- S-S  ----->  S-S
@@ -195,32 +194,42 @@ local reduction_rules = {
     --   Z            I
     -- S-S  ----->  S-S
     -- Z            I
-    {
-      "z\nswap,swap\n?,z",
-      ",,\ntrue,2,",
-      2,
-      5
-    }
+    "z\nswap,swap\n?,z|,,\ntrue,2,|2|5",
+
+    -- Z            I
+    -- H X          H I
+    -- X-C  ----->  X-C
+    -- H X          H I
+    --
+    --   Z            I
+    -- X H          I H
+    -- C-X  ----->  C-X
+    -- X H          I H
+    "z\nh,x\ncnot_x,control\nh,x|,,\ntrue,1,\ntrue,3,|3|10",
+
+    -- Z            I
+    -- H            H
+    -- X-C  ----->  X-C
+    -- H            H
+    -- Z            I
+    --
+    --   Z            I
+    --   H            H
+    -- C-X  ----->  C-X
+    --   H            H
+    --   Z            I
+    "z\nh\ncnot_x,control\nh\nz|,,\n,4,|2|10"
   },
 
   s = {
     -- S          I
     -- S  ----->  Z
-    {
-      "s\ns",
-      ",,\n,1,z",
-      2
-    },
+    "s\ns|,,\n,1,z|2|",
 
     -- S          I
     -- Z          I
     -- S  ----->  X
-    {
-      "s\nz\ns",
-      ",,\n,1,\n,2,z",
-      3,
-      3
-    },
+    "s\nz\ns|,,\n,1,\n,2,z|3|3",
 
     -- S            Z
     -- S-S  ----->  S-S
@@ -229,54 +238,52 @@ local reduction_rules = {
     --   S            Z
     -- S-S  ----->  S-S
     -- S            I
-    {
-      "s\nswap,swap\n?,s",
-      ",,z\ntrue,2,",
-      2,
-      5
-    }
+    "s\nswap,swap\n?,s|,,z\ntrue,2,|2|5",
+
+    -- S            I
+    -- Z            Z
+    -- S-S  ----->  S-S
+    --   S            I
+    --
+    --   S            I
+    --   Z            Z
+    -- S-S  ----->  S-S
+    -- S            I
+    "s\nz\nswap,swap\n?,s|,,\n,1,z\ntrue,3,|3|7",
+
+    -- S            Z
+    -- S-S  ----->  S-S
+    --   Z            I
+    --   S            I
+    --
+    --   S            Z
+    -- S-S  ----->  S-S
+    -- Z            I
+    -- S            I
+    "s\nswap,swap\n?,z\n?,s|,,z\ntrue,2,\ntrue,3,|3|7"
   },
 
   t = {
     -- T          I
     -- T  ----->  S
-    {
-      "t\nt",
-      ",,\n,1,s",
-      2
-    },
+    "t\nt|,,\n,1,s|2|",
 
     -- T          I
     -- S          I
     -- T  ----->  Z
-    {
-      "t\ns\nt",
-      ",,\n,1,\n,2,z",
-      3,
-      3
-    },
+    "t\ns\nt|,,\n,1,\n,2,z|3|3",
 
     -- T          I
     -- Z          I
     -- S          I
     -- T  ----->  I
-    {
-      "t\nz\ns\nt",
-      ",,\n,1,\n,2,\n,3,",
-      4,
-      4
-    },
+    "t\nz\ns\nt|,,\n,1,\n,2,\n,3,|4|4",
 
     -- T          I
     -- S          I
     -- Z          I
     -- T  ----->  I
-    {
-      "t\ns\nz\nt",
-      ",,\n,1,\n,2,\n,3,",
-      4,
-      4
-    },
+    "t\ns\nz\nt|,,\n,1,\n,2,\n,3,|4|4",
 
     -- T            S
     -- S-S  ----->  S-S
@@ -285,12 +292,55 @@ local reduction_rules = {
     --   T            S
     -- S-S  ----->  S-S
     -- T            I
-    {
-      "t\nswap,swap\n?,t",
-      ",,s\ntrue,2,",
-      2,
-      5
-    }
+    "t\nswap,swap\n?,t|,,s\ntrue,2,|2|5",
+
+    -- T            I
+    -- S            Z
+    -- S-S  ----->  S-S
+    --   T            I
+    --
+    --   T            I
+    --   S            Z
+    -- S-S  ----->  S-S
+    -- S            I
+    "t\ns\nswap,swap\n?,t|,,\n,1,z\ntrue,3,|3|7",
+
+    -- T            Z
+    -- S-S  ----->  S-S
+    --   S            I
+    --   T            I
+    --
+    --   T            Z
+    -- S-S  ----->  S-S
+    -- S            I
+    -- T            I
+    "t\nswap,swap\n?,s\n?,t|,,z\ntrue,2,\ntrue,3,|3|7",
+
+    -- T            I
+    -- S-S  ----->  S-S
+    --   Z            I
+    --   S            I
+    --   T            I
+    --
+    --   T            I
+    -- S-S  ----->  S-S
+    -- Z            I
+    -- S            I
+    -- T            I
+    "t\nswap,swap\n?,z\n?,s\n?,t|,,\ntrue,2,\ntrue,3,\ntrue,4,|4|8",
+
+    -- T            I
+    -- S-S  ----->  S-S
+    --   S            I
+    --   Z            I
+    --   T            I
+    --
+    --   T            I
+    -- S-S  ----->  S-S
+    -- S            I
+    -- Z            I
+    -- T            I
+    "t\nswap,swap\n?,s\n?,z\n?,t|,,\ntrue,2,\ntrue,3,\ntrue,4,|4|8"
   },
 
   control = {
@@ -299,12 +349,7 @@ local reduction_rules = {
     --
     -- X-C          I I
     -- X-C  ----->  I I
-    {
-      "control,cnot_x\ncontrol,cnot_x",
-      ",,\ntrue,,\n,1,\ntrue,1,",
-      4,
-      5
-    },
+    "control,cnot_x\ncontrol,cnot_x|,,\ntrue,,\n,1,\ntrue,1,|4|5",
 
     -- C-X          I I
     -- X-C          I I
@@ -313,52 +358,39 @@ local reduction_rules = {
     -- X-C          I I
     -- C-X          I I
     -- X-C  ----->  S-S
-    {
-      "control,cnot_x\ncnot_x,control\ncontrol,cnot_x",
-      ",,\ntrue,,\n,1,\ntrue,1,\n,2,swap\ntrue,2,swap",
-      6,
-      10
-    },
+    "control,cnot_x\ncnot_x,control\ncontrol,cnot_x|,,\ntrue,,\n,1,\ntrue,1,\n,2,swap\ntrue,2,swap|6|10",
 
     --  C-X          I I
     --  S-S  ----->  S-S
     --  X-C          I I
-    {
-      "control,cnot_x\nswap,swap\ncnot_x,control",
-      ",,\ntrue,,\n,2,\ntrue,2,",
-      4,
-      10
-    }
+    "control,cnot_x\nswap,swap\ncnot_x,control|,,\ntrue,,\n,2,\ntrue,2,|4|10"
   },
 
   swap = {
     -- S-S          I
     -- S-S  ----->  I
-    {
-      "swap,swap\nswap,swap",
-      ",,\ntrue,,\n,1,\ntrue,1,",
-      4,
-      30
-    }
+    "swap,swap\nswap,swap|,,\ntrue,,\n,1,\ntrue,1,|4|30"
   }
 }
 
 for first_gate, rules in pairs(reduction_rules) do
-  foreach(reduction_rules[first_gate], function(rule)
-    rule[1] = transform(split(rule[1], "\n"), split)
-    rule[2] = transform(split(rule[2], "\n"), function(to)
-      local attrs = split(to)
-
-      -- 簡約でできるゲートはすべて異なるオブジェクトでなくてはならないので、
-      -- ルールにはゲートオブジェクトを入れるのではなく、
-      -- ゲートのタイプ (gate_type) を入れ、board 側で new する
-      return {
-        dx = attrs[1] ~= "",
-        dy = attrs[2] == "" and nil or tonum(attrs[2]),
-        gate_type = attrs[3] == "" and 'i' or attrs[3]
-      }
-    end)
-  end)
+  for i, each in pairs(reduction_rules[first_gate]) do
+    local pattern, reduce_to, gate_count, score = unpack(split(each, "|"))
+    ---@diagnostic disable-next-line: assign-type-mismatch
+    reduction_rules[first_gate][i] = {
+      transform(split(pattern, "\n"), split),
+      transform(split(reduce_to, "\n"), function(to)
+        local attrs = split(to)
+        return {
+          dx = attrs[1] ~= "",
+          dy = attrs[2] == "" and nil or tonum(attrs[2]),
+          gate_type = attrs[3] == "" and 'i' or attrs[3]
+        }
+      end),
+      tonum(gate_count),
+      tonum(score)
+    }
+  end
 end
 
 return reduction_rules
