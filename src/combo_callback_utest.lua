d44c300394e9b8cac0d9ee5c94c05ceb37f86767
Yasuhito Takamiya <yasuhito@gmail.com>
2022-10-25 10:04:05 +0900

    6-コンボのテストを追加

--- src/combo_callback_utest.lua
+++ src/combo_callback_utest.lua
@@ -31,7 +31,7 @@ describe('コンボ (同時消し) のコールバック', function()
     combo_callback.was_called_with(4, 2, 11, match._, match._)
   end)
 
-  it("5-コンボ発生でコールバックが呼ばれる #solo", function()
+  it("5-コンボ発生でコールバックが呼ばれる", function()
     --    S            S <- 5-combo
     -- [Z H]         H Z
     --  H S  ----->  H S
@@ -49,5 +49,26 @@ describe('コンボ (同時消し) のコールバック', function()
 
     combo_callback.was_called(1)
     combo_callback.was_called_with(5, 2, 10,  match._, match._)
+  end)
+
+  it("6-コンボ発生でコールバックが呼ばれる #solo", function()
+    -- [S H]        H S <- 6-combo
+    --  X Z         Z X
+    --  H S  -----> H S
+    board:put(1, 10, s_gate())
+    board:put(2, 10, h_gate())
+    board:put(1, 11, x_gate())
+    board:put(2, 11, z_gate())
+    board:put(1, 12, h_gate())
+    board:put(2, 12, s_gate())
+
+    board:swap(1, 10)
+    wait_swap_to_finish(board)
+
+    local combo_callback = assert.spy(game.combo_callback)
+    board:update(game.combo_callback)
+
+    combo_callback.was_called(1)
+    combo_callback.was_called_with(6, 2, 10,  match._, match._)
   end)
 end)
