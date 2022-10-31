a803320e75ce0ff12ce2d80854dbfdc35053e65e
Yasuhito Takamiya <yasuhito@gmail.com>
2022-11-01 05:04:56 +0900

    攻撃キューブの演出を修正 (#38)

--- src/attack_cube.lua
+++ src/attack_cube.lua
@@ -1,14 +1,6 @@
 ---@diagnostic disable: lowercase-global
 
-local all_cubes, apex, data = {}, {}, split("122413345657687815263748", 1)
-
-for x = -4, 4, 8 do
-  for y = -4, 4, 8 do
-    for z = -4, 4, 8 do
-      add(apex, { x = x, y = y, z = z })
-    end
-  end
-end
+local all_cubes, data = {}, split("122413345657687815263748", 1)
 
 function update_attack_cubes()
   foreach(all_cubes, function(each)
@@ -41,18 +33,26 @@ function render_attack_cubes()
       if i % 2 > 0 then
         line()
       end
-      local f = apex[data[i]]
+      local f, size = apex[data[i]], _count / 2
       local x, y, z = f.x, f.y, f.z + f.x * .0125
       f.x, f.y, f.z = x - f.z * .0125, y - z * .0125, z + y * .0125
-      line(f.x + _x, f.y + _y, cube_color)
+      line(f.x * size + _x, f.y * size + _y, cube_color)
     end
   end)
 end
 
-function create_attack_cube(x, y, callback, target_x, target_y, left)
+function create_attack_cube(count, x, y, callback, target_x, target_y, left)
   local _ENV = setmetatable({}, { __index = _ENV })
 
-  _x, _y, _callback, _target_x, _target_y, _tick, _left = x, y, callback, target_x, target_y, 0, left ~= nil
+  _count, _x, _y, _callback, _target_x, _target_y, _tick, _left, apex = count, x, y, callback, target_x, target_y, 0, left ~= nil, {}
+
+  for x = -4, 4, 8 do
+    for y = -4, 4, 8 do
+      for z = -4, 4, 8 do
+        add(apex, { x = x, y = y, z = z })
+      end
+    end
+  end
 
   add(all_cubes, _ENV)
 end
