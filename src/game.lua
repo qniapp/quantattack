110b7133d4bbfef019a18b4c07730a8981a48ddd
Yasuhito Takamiya <yasuhito@gmail.com>
2022-11-01 03:54:42 +0900

    おじゃまが降る条件を少し厳しくする

--- src/game.lua
+++ src/game.lua
@@ -19,7 +19,9 @@ function game.combo_callback(combo_count, x, y, player, board, other_board)
   local attack_cube_callback = function()
     player.score = player.score + combo_count
     local b = other_board or board
-    b:fall_garbage()
+    if combo_count > 4 then
+      b:fall_garbage()
+    end
   end
 
   create_bubble("combo", combo_count, board:screen_x(x), board:screen_y(y))
@@ -33,7 +35,9 @@ function game.chain_callback(chain_count, x, y, player, board, other_board)
     local attack_cube_callback = function()
       player.score = player.score + (chain_bonus[chain_count] or 180)
       local b = other_board or board
-      b:fall_garbage()
+      if chain_count > 2 then
+        b:fall_garbage()
+      end
     end
 
     create_bubble("chain", chain_count, board:screen_x(x), board:screen_y(y))
