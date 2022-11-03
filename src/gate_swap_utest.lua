f9db47047cce231cbaca5b8a5e63ea2b5bd931dc
Yasuhito Takamiya <yasuhito@gmail.com>
2022-11-04 04:41:56 +0900

    テストを追加

--- src/gate_swap_utest.lua
+++ src/gate_swap_utest.lua
@@ -9,6 +9,45 @@ describe('ゲートの入れ替え', function()
     board = create_board()
   end)
 
+  describe('フレーム数', function()
+    it("入れ替えると状態が swapping になる #solo", function()
+      board:put(1, 12, h_gate())
+      board:put(2, 12, x_gate())
+
+      board:swap(1, 12)
+
+      assert.is_true(board:gate_at(1, 12):is_swapping())
+    end)
+
+    it("4 フレームで入れ替わる #solo", function()
+      board:put(1, 12, h_gate())
+      board:put(2, 12, x_gate())
+
+      -- swap 開始フレーム
+      board:swap(1, 12)
+      board:update()
+      assert.is_true(board:gate_at(1, 12):is_swapping())
+      assert.is_true(board:gate_at(2, 12):is_swapping())
+
+      board:update()
+      assert.is_true(board:gate_at(1, 12):is_swapping())
+      assert.is_true(board:gate_at(2, 12):is_swapping())
+
+      board:update()
+      assert.is_true(board:gate_at(1, 12):is_swapping())
+      assert.is_true(board:gate_at(2, 12):is_swapping())
+
+      board:update()
+      assert.is_true(board:gate_at(1, 12):is_swapping())
+      assert.is_true(board:gate_at(2, 12):is_swapping())
+
+      board:update()
+
+      assert.is_true(board:gate_at(1, 12):is_idle())
+      assert.is_true(board:gate_at(2, 12):is_idle())
+    end)
+  end)
+
   describe('I ゲートとの入れ替え', function()
     --
     -- [H ] (H と I を入れ換え)
