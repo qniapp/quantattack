08686cc117d2320a69c27a96c670fe5cd4813cdd
Yasuhito Takamiya <yasuhito@gmail.com>
2022-11-03 11:19:18 +0900

    solo の cart を分離 (#42)

---
+++ build_solo.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./scripts/build_cartridge.sh src main_solo.lua src -o build/solo --data data.p8 -m 1
---
+++ src/app/solo.lua
@@ -0,0 +1,16 @@
+require("engine/application/constants")
+require("engine/core/class")
+
+local gameapp = require("engine/application/gameapp")
+local app = derived_class(gameapp)
+local solo = require("solo")
+
+function app:_init()
+  gameapp._init(self, fps60)
+end
+
+function app.instantiate_gamestates()
+  return { solo() }
+end
+
+return app
