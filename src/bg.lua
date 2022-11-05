9f367983eb753a4c024b38e49dca15d59e4aef96
Yasuhito Takamiya <yasuhito@gmail.com>
2022-11-05 10:03:42 +0900

    リファクタリング

--- src/bg.lua
+++ src/bg.lua
@@ -1,25 +1,21 @@
-local bgtime = 0
+local plasma_time = 0
 
--- ???: https://pico-8.fandom.com/wiki/Memory#Screen_data
 function _plasma_draw()
-  bgtime = bgtime + 0.3
+  plasma_time = plasma_time + 0.05
 
-  local t = bgtime / 12
+  local t = plasma_time / 12
   local sint, cost = sin(t / 15) / 120, cos(t / 17) / 180
 
   for x = 0, 31 do
     local xsint = x * sint
 
     for y = 0, 31 do
-      if x == 0 or x == 31 or (12 < x and x < 20) or y < 8 then
-        local v = sin((xsint + y * cost) * 8 + t)
-        v = v * cos(x / 53 + y / 57)
-        v = flr((v + 1) * 2.5)
+      local v = sin((xsint + y * cost) * 8 + t)
+      v = flr((v * cos(x / 53 + y / 57) + 1) * 2.5)
 
-        local plasma_colors = { 0, 13, 1, 13, 1 }
-        local _x, _y = x << 2, y << 2
-        rectfill(_x, _y, _x + 1, _y + 1, plasma_colors[v + 1])
-      end
+      local plasma_colors = { 0, 5, 1, 5, 1 }
+      local _x, _y = x << 2, y << 2
+      rect(_x, _y, _x + 1, _y + 1, plasma_colors[v + 1])
     end
   end
 end
