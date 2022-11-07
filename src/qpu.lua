e02c76d5c620731ab9f484eefe71f1d942d1a5a9
Yasuhito Takamiya <yasuhito@gmail.com>
2022-11-08 04:49:42 +0900

    難易度調整

--- src/qpu.lua
+++ src/qpu.lua
@@ -10,6 +10,7 @@ function create_qpu(cursor)
     end,
 
     update = function(_ENV, board)
+      -- QPU の入力
       left, right, up, down, x, o = false, false, false, false, false, false
 
       local next_action = commands[1]
@@ -18,51 +19,25 @@ function create_qpu(cursor)
         del(commands, next_action)
         _ENV[next_action] = true
       else
-        if board:top_gate_y() > 5 then
+        if board:top_gate_y() > 8 then
           add_raise_command(_ENV)
           return
         end
 
-        -- 上から探す
-        for new_y = 2, board.rows - 1 do
+        for new_y = 2, board.rows do
           for new_x = 1, board.cols - 1 do
-            local left_gate = board:reducible_gate_at(new_x, new_y)
-            local right_gate = board:reducible_gate_at(new_x + 1, new_y)
+            local left_gate, right_gate = board:reducible_gate_at(new_x, new_y),
+                board:reducible_gate_at(new_x + 1, new_y)
 
-            -- TODO: left_gate がおじゃまゲートの一部だった場合もはじく
             if not (left_gate:is_idle() and right_gate:is_idle()) then
               goto next_gate
             end
 
-            -- 入れ替えることで右に落とせる場合
+            -- 下の X-C を左にずらして消す。
             --
-            -- [X ]
-            --  H
-            if left_gate:is_single_gate() and board:is_gate_empty(new_x + 1, new_y) and
-                board:is_gate_empty(new_x + 1, new_y + 1) then
-              move_and_swap(_ENV, new_x, new_y)
-              return
-            end
-
-            -- 以下の形をみつけたら、上の X-C を左にずらして消す。
-            -- (QPU は X-C をどんどん右にずらすので、この形は頻発する)
-            --
-            -- [  X]-C
             --  X-C
+            -- [  X]-C
             if board:is_gate_empty(new_x, new_y) and right_gate:is_cnot_x() and right_gate.other_x == new_x + 2 and
-                board:reducible_gate_at(new_x, new_y + 1):is_cnot_x() and
-                board:reducible_gate_at(new_x, new_y + 1).other_x == new_x + 1 then
-              move_and_swap(_ENV, new_x, new_y)
-              move_and_swap(_ENV, new_x + 1, new_y)
-              return
-            end
-
-            -- 同様に下の X-C を左にずらして消す。
-            --
-            --  X-C
-            -- [  X]-C
-            if new_y > 1 and
-                board:is_gate_empty(new_x, new_y) and right_gate:is_cnot_x() and right_gate.other_x == new_x + 2 and
                 board:reducible_gate_at(new_x, new_y - 1):is_cnot_x() and
                 board:reducible_gate_at(new_x, new_y - 1).other_x == new_x + 1 then
               move_and_swap(_ENV, new_x, new_y)
@@ -88,41 +63,71 @@ function create_qpu(cursor)
               return
             end
 
-            -- 入れ換えると右側がそろう場合
-            --
-            -- [X  ]
-            --  ■ X
-            if left_gate:is_single_gate() and
-                (right_gate:is_single_gate() or board:is_gate_empty(new_x + 1, new_y)) and
-                left_gate.type == board:reducible_gate_at(new_x + 1, new_y + 1).type then
-              move_and_swap(_ENV, new_x, new_y)
-              return
-            end
+            if new_y < board.rows then
+              -- 以下の形をみつけたら、上の X-C を左にずらして消す。
+              -- (QPU は X-C をどんどん右にずらすので、この形は頻発する)
+              --
+              -- [  X]-C
+              --  X-C
+              if board:is_gate_empty(new_x, new_y) and right_gate:is_cnot_x() and right_gate.other_x == new_x + 2 and
+                  board:reducible_gate_at(new_x, new_y + 1):is_cnot_x() and
+                  board:reducible_gate_at(new_x, new_y + 1).other_x == new_x + 1 then
+                move_and_swap(_ENV, new_x, new_y)
+                move_and_swap(_ENV, new_x + 1, new_y)
+                return
+              end
 
-            -- 入れ換えると左側がそろう場合
-            --
-            -- [  X]
-            --  X ■
-            if (left_gate:is_single_gate() or board:is_gate_empty(new_x, new_y)) and
-                right_gate:is_single_gate() and
-                right_gate.type == board:reducible_gate_at(new_x, new_y + 1).type then
-              move_and_swap(_ENV, new_x, new_y)
-              return
-            end
-
-            -- 連続して左に移動すると落とせる場合
-            -- 手数のかかる操作なので、優先度は一番下
-            --
-            --  [ X]
-            --   HH
-            if board:is_gate_empty(new_x, new_y) and right_gate:is_single_gate() then
-              for i = new_x, 1, -1 do
-                if not board:is_gate_empty(i, new_y) then
-                  goto next_gate
-                end
-                if board:is_gate_empty(i, new_y + 1) then
+              if left_gate:is_single_gate() then
+                -- 入れ替えることで右に落とせる場合
+                --
+                -- [X ]
+                --  H
+                if board:is_gate_empty(new_x + 1, new_y) and
+                    board:is_gate_empty(new_x + 1, new_y + 1) then
                   move_and_swap(_ENV, new_x, new_y)
                   return
+                end
+
+                -- 入れ換えると右側がそろう場合
+                --
+                -- [X  ]
+                --  ■ X
+                if (right_gate:is_single_gate() or board:is_gate_empty(new_x + 1, new_y)) and
+                    left_gate.type == board:reducible_gate_at(new_x + 1, new_y + 1).type then
+                  move_and_swap(_ENV, new_x, new_y)
+                  return
+                end
+              end
+
+              if right_gate:is_single_gate() then
+                -- 連続して左に移動すると落とせる場合
+                --
+                --  [ X]
+                --   HH
+                if board:is_gate_empty(new_x, new_y) then
+                  for i = new_x, 1, -1 do
+                    if not board:is_gate_empty(i, new_y) then
+                      goto next_gate
+                    end
+                    if board:is_gate_empty(i, new_y + 1) then
+                      move_and_swap(_ENV, new_x, new_y)
+                      return
+                    end
+                  end
+                end
+
+                -- 連続して左に移動すると消せる場合
+                --
+                --  [ X]
+                --  XHH
+                for i = new_x, 1, -1 do
+                  if not board.gates[i][new_y]:is_single_gate() then
+                    goto next_gate
+                  end
+                  if board.gates[i][new_y + 1].type == right_gate.type then
+                    move_and_swap(_ENV, new_x, new_y)
+                    return
+                  end
                 end
               end
             end
