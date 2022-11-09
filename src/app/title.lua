00dff452deec2a466cea5f373f161c0a826edb26
Yasuhito Takamiya <yasuhito@gmail.com>
2022-11-09 09:06:53 +0900

    トークン削減

--- src/app/title.lua
+++ src/app/title.lua
@@ -1,6 +1,6 @@
 require("engine/core/class")
 
-local gameapp = require("engine/application/gameapp")
+local gameapp = require("app/gameapp")
 local app = derived_class(gameapp)
 
 local title = require("title")
