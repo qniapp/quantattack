1bcaff328c71f228d2c77ce853d3a0fc064db32f
Yasuhito Takamiya <yasuhito@gmail.com>
2022-11-14 12:50:11 +0900

    QPU vs QPU モードで経過時間を表示 (#53)

--- src/qpu_vs_qpu.lua
+++ src/qpu_vs_qpu.lua
@@ -37,32 +37,29 @@ function qpu_vs_qpu:on_enter()
 end
 
 function qpu_vs_qpu:update()
-  if qpu1_board:is_game_over() or qpu2_board:is_game_over() then
-    if qpu1_board.lose then
-      qpu2_board.win = true
-    end
-    if qpu2_board.lose then
-      qpu1_board.win = true
-    end
+  game:update()
 
-    if not game_over_time then
-      game_over_time = time()
-    else
-      if time() - game_over_time > 2 then
-        qpu1_board.push_any_key = true
-        qpu2_board.push_any_key = true
-        if btn(4) or btn(5) then -- x または z でタイトルへ戻る
-          load('qitaev_title')
-        end
+  if game:is_game_over() then
+    if t() - game.game_over_time > 2 then
+      qpu1_board.push_any_key = true
+      qpu2_board.push_any_key = true
+      if btnp(4) or btnp(5) then -- x または z でタイトルへ戻る
+        load('qitaev_title')
       end
     end
   end
-
-  game:update()
 end
 
-function qpu_vs_qpu:render() -- override
+-- 画面の描画
+function qpu_vs_qpu:render()
   game:render()
+
+  -- 経過時間の表示
+  cursor(57, 106)
+  print("time")
+
+  cursor(55, 114)
+  print(game:elapsed_time_string())
 end
 
 return qpu_vs_qpu
