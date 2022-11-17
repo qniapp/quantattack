3408b4c71ccf5b2d88cc11124d1dbe493a156dd5
Yasuhito Takamiya <yasuhito@gmail.com>
2022-11-17 16:10:47 +0900

    リファクタリング

--- src/i_gate.lua
+++ src/i_gate.lua
@@ -3,6 +3,7 @@ function i_gate()
   local i = setmetatable({
     gate_swap_animation_frame_count = 4,
 
+    -- TODO: gate ベースクラスから継承して、ここには何も書かなくていいようにする
     _init = function(_ENV)
       _state = "idle"
     end,
@@ -13,37 +14,45 @@ function i_gate()
 
     update = function(_ENV)
       if is_idle(_ENV) then
-        -- NOP
+        _update_idle(_ENV)
       elseif is_swapping(_ENV) then
-        -- TODO: update_swap() とする
-        -- TODO: そもそもちゃんと 4 フレームで終わってるか確認
-        if _tick_swap < gate_swap_animation_frame_count then
-          _tick_swap = _tick_swap + 1
+        _update_swap(_ENV)
+      end
+    end,
+
+    -- TODO: gate ベースクラスから継承して、ここには何も書かなくていいようにする
+    _update_idle = function(_ENV)
+    end,
+
+    _update_swap = function(_ENV)
+      -- TODO: update_swap() とする
+      -- TODO: そもそもちゃんと 4 フレームで終わってるか確認
+      if _tick_swap < gate_swap_animation_frame_count then
+        _tick_swap = _tick_swap + 1
+      else
+        -- SWAP 完了
+        local new_x = x + 1
+        local orig_x = x
+        local right_gate = board.gates[new_x][y]
+
+        -- FIXME: この処理はそもそも board 側でやるのが正しい
+        board:put(new_x, y, _ENV)
+        board:put(orig_x, y, right_gate)
+
+        if other_x == nil and right_gate.other_x == nil then -- 1.
+          -- NOP
+        elseif not is_i(_ENV) and right_gate:is_i() then -- 2.
+          board.gates[other_x][y].other_x = new_x
+        elseif is_i(_ENV) and not right_gate:is_i() then -- 3.
+          board.gates[right_gate.other_x][y].other_x = orig_x
+        elseif other_x and right_gate.other_x then -- 4.
+          other_x, right_gate.other_x = orig_x, new_x
         else
-          -- SWAP 完了
-          local new_x = x + 1
-          local orig_x = x
-          local right_gate = board.gates[new_x][y]
-
-          -- FIXME: この処理はそもそも board 側でやるのが正しい
-          board:put(new_x, y, _ENV)
-          board:put(orig_x, y, right_gate)
-
-          if other_x == nil and right_gate.other_x == nil then -- 1.
-            -- NOP
-          elseif not is_i(_ENV) and right_gate:is_i() then -- 2.
-            board.gates[other_x][y].other_x = new_x
-          elseif is_i(_ENV) and not right_gate:is_i() then -- 3.
-             board.gates[right_gate.other_x][y].other_x = orig_x
-          elseif other_x and right_gate.other_x then -- 4.
-            other_x, right_gate.other_x = orig_x, new_x
-          else
-            assert(false, "we should not reach here")
-          end
-
-          change_state(_ENV, "idle")
-          right_gate:change_state("idle")
+          assert(false, "we should not reach here")
         end
+
+        change_state(_ENV, "idle")
+        right_gate:change_state("idle")
       end
     end,
 
