780eb82e02ea7b625ccd86a38f256eb9c643c687
Yasuhito Takamiya <yasuhito@gmail.com>
2022-11-23 14:46:11 +0900

    リップル効果を追加

---
+++ src/ripple.lua
@@ -0,0 +1,32 @@
+ripple_speed = nil
+
+local t1, t2, tick
+
+function init_ripple()
+  t1, t2, tick, ripple_speed = 0, 0, 0, "normal"
+end
+
+function update_ripple()
+  tick = tick + 1
+
+  if ripple_speed == "normal" then
+    t1 = t1 - 1 / 1500
+    t2 = t2 - 1 / 150
+  else
+    t1 = t1 - 1 / 3000
+    t2 = t2 - 1 / 300
+  end
+end
+
+function render_ripple()
+  for i = -5, 5 do
+    for j = -5, 5 do
+      local ang = atan2(i, j)
+      local d = sqrt(i * i + j * j)
+      local r = 2 + 2 * sin(d / 4 + t2)
+      local h = 3 * r
+      local clr = (ripple_speed == "slow" and r > 3 and tick % 2 == 0) and 13 or 1
+      circfill(64 + 12 * d * cos(ang + t1), 64 + 12 * d * sin(ang + t1) - h, r, clr)
+    end
+  end
+end
