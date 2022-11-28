6de544f11e703ba22f1039652310fe592c1442f8
Yasuhito Takamiya <yasuhito@gmail.com>
2022-11-29 04:33:49 +0900

    lib/class への依存をなくす

--- src/app/qpu_vs_qpu.lua
+++ src/app/qpu_vs_qpu.lua
@@ -1,15 +1,23 @@
-require("lib/class")
-
 local gameapp = require("app/gameapp")
-local app = derived_class(gameapp)
 local qpu_vs_qpu = require("qpu_vs_qpu/qpu_vs_qpu")
 
-function app:_init()
-  gameapp._init(self)
-end
+function app_qpu_vs_qpu()
+  local _app = {}
+  _app.__index = _app
 
-function app.instantiate_gamestates()
-  return { qpu_vs_qpu() }
-end
+  setmetatable(_app, {
+    __index = gameapp,
+  })
 
-return app
+  function _app:_init()
+    gameapp:_init()
+  end
+
+  function _app:instantiate_gamestates()
+    return { qpu_vs_qpu() }
+  end
+
+  _app:_init()
+
+  return _app
+end
--- src/main_qpu_vs_qpu.lua
+++ src/main_qpu_vs_qpu.lua
@@ -1,7 +1,5 @@
--- must require at main top, to be used in any required modules from here
-require("engine/pico8/api")
+require("app/qpu_vs_qpu")
 
-local app_qpu_vs_qpu = require("app/qpu_vs_qpu")
 local app = app_qpu_vs_qpu()
 
 function _init()
