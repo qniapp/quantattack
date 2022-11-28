ef91d8db4ac7ed204359f84fe80ca6263e86d96c
Yasuhito Takamiya <yasuhito@gmail.com>
2022-11-29 04:07:55 +0900

    lib/class への依存をなくす

--- src/app/title.lua
+++ src/app/title.lua
@@ -1,35 +1,41 @@
-require("lib/class")
-
 local gameapp = require("app/gameapp")
-local app = derived_class(gameapp)
-
 local title_demo = require("title/title_demo")
 local title_menu = require("title/title_menu")
 
--- local game_class = require("title/game")
 require("title/game")
 demo_game = game()
 
-local qpu_board = create_board(0, 16)
-local qpu_cursor = create_player_cursor(qpu_board)
-local qpu = create_qpu(qpu_cursor, qpu_board)
+function app_title()
+  local _app = {}
+  _app.__index = _app
 
-function app:_init()
-  gameapp._init(self)
+  setmetatable(_app, {
+    __index = gameapp,
+  })
 
-  qpu:init()
-  qpu_board:put_random_gates()
-  qpu_cursor:init()
+  function _app:_init()
+    local qpu_board = create_board(0, 16)
+    local qpu_cursor = create_player_cursor(qpu_board)
+    local qpu = create_qpu(qpu_cursor, qpu_board)
 
-  qpu_board.show_wires = false
-  qpu_board.show_top_line = false
+    gameapp:_init()
 
-  demo_game:init()
-  demo_game:add_player(qpu, qpu_cursor, qpu_board)
+    qpu:init()
+    qpu_board:put_random_gates()
+    qpu_cursor:init()
+
+    qpu_board.show_wires = false
+    qpu_board.show_top_line = false
+
+    demo_game:init()
+    demo_game:add_player(qpu, qpu_cursor, qpu_board)
+  end
+
+  function _app:instantiate_gamestates()
+    return { title_demo(), title_menu() }
+  end
+
+  _app:_init()
+
+  return _app
 end
-
-function app.instantiate_gamestates()
-  return { title_demo(), title_menu() }
-end
-
-return app
--- src/main_title.lua
+++ src/main_title.lua
@@ -1,8 +1,8 @@
 -- we must require engine/pico8/api at the top of our main.lua, so API bridges apply to all modules
 require("engine/pico8/api")
 
-local app_class = require("app/title")
-local app = app_class()
+require("app/title")
+local app = app_title()
 
 function _init()
   app.initial_gamestate = ':title_demo'
