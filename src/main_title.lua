69af3b8bcb10e9254310492ca3036617adeab51c
Yasuhito Takamiya <yasuhito@gmail.com>
2022-11-29 05:22:13 +0900

    リファクタリング

--- src/main_title.lua
+++ src/main_title.lua
@@ -33,20 +33,3 @@ function _draw()
   cls()
   flow:render()
 end
-
--- require("app/title")
-
--- local app = app_title()
-
--- function _init()
---   app.initial_gamestate = ':title_demo'
---   app:start()
--- end
-
--- function _update60()
---   app:update()
--- end
-
--- function _draw()
---   app:draw()
--- end
