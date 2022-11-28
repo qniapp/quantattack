3010b8bfe7f46eb31618f01ebdc97274ac021538
Yasuhito Takamiya <yasuhito@gmail.com>
2022-11-28 16:04:15 +0900

    トークン削減

--- src/lib/attack_bubble.lua
+++ src/lib/attack_bubble.lua
@@ -10,10 +10,10 @@ function update_attack_bubbles()
 
     if abs(_target_x - _x) < 5 and abs(_target_y - _y) < 5 then
       del(all_bubbles, each)
-      each._callback(_target_x, _target_y)
+      _callback(_target_x, _target_y)
     end
 
-    if _tick < 50 then
+    if t() - _start_time < 0.8 then
       if slow_attack_bubbles and #all_bubbles > 0 then
         flip()
       end
@@ -22,18 +22,17 @@ function update_attack_bubbles()
       _dx, _dy = (_target_x - _x) / 6, (_target_y - _y) / 6
     end
 
-    _x, _y, _tick, _angle = _x + _dx, _y + _dy, _tick + 1, _angle + 0.06
+    _x, _y = _x + _dx, _y + _dy
   end)
 end
 
 function render_attack_bubbles()
   foreach(all_bubbles, function(each)
-    local _ENV = each
+    local _ENV, _angle = each, t()
 
     fillp(23130.5)
     circfill(_x, _y, 6 + 2 * sin(1.5 * _angle), 0x0c)
     fillp()
-
     circfill(_x, _y, 4 + 2 * sin(2 * _angle), 12)
     circfill(_x, _y, 3 + sin(2.5 * _angle), 7)
   end)
@@ -42,8 +41,7 @@ end
 function create_attack_bubble(x, y, callback, target_x, target_y)
   local _ENV = setmetatable({}, { __index = _ENV })
 
-  _x, _y, _callback, _target_x, _target_y, _tick, _angle, _left = x, y, callback, target_x, target_y, 0, 0, x > 64
-
+  _x, _y, _callback, _target_x, _target_y, _start_time, _left = x, y, callback, target_x, target_y, t(), x > 64
   add(all_bubbles, _ENV)
   sfx(11)
 end
