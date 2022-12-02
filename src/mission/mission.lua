7c86e3d395bbf55813ac548773f1fe9ff7008204
Yasuhito Takamiya <yasuhito@gmail.com>
2022-12-03 06:39:14 +0900

    マッチ中だけリップルの色を変える

--- src/mission/mission.lua
+++ src/mission/mission.lua
@@ -145,37 +145,6 @@ local waves = {
   }
 }
 
-local all_match_circles = {}
-
-local function create_match_circle(x, y)
-  add(all_match_circles, { x = x, y = y, r = 0, c = 7 })
-  add(all_match_circles, { x = x, y = y, r = 2, c = 13, pattern = 23130.5 })
-end
-
-local function update_match_circles()
-  for _, each in pairs(all_match_circles) do
-    local dr = 4
-    if attack_bubble.slow then
-      dr = 0.8
-    end
-    each.r = each.r + dr
-
-    if each.r > 128 then
-      del(all_match_circles, each)
-    end
-  end
-end
-
-local function render_match_circles()
-  for _, each in pairs(all_match_circles) do
-    if each.pattern then
-      fillp(each.pattern)
-    end
-    circ(each.x, each.y, each.r, each.c)
-    fillp()
-  end
-end
-
 state = ":play"
 match_pattern = nil
 match_screen_x = nil
@@ -203,7 +172,7 @@ function mission_game.reduce_callback(score, x, y, player, pattern, dx)
       match_pattern = each.rule
       match_screen_x = board:screen_x(x)
       match_screen_y = board:screen_y(y)
-      create_match_circle(board:screen_x(x) + 3, board:screen_y(y) + 3)
+      -- create_match_circle(board:screen_x(x) + 3, board:screen_y(y) + 3)
       attack_bubble:create(board:screen_x(x), board:screen_y(y), attack_cube_callback, each.x, each.y)
     end
   end
@@ -249,8 +218,6 @@ function mission:update()
     end
   end
 
-  update_match_circles()
-
   if #all_balloons == 0 then
     wave_number = wave_number + 1
     local current_wave = waves[wave_number]
@@ -271,6 +238,8 @@ end
 function mission:render() -- override
   if state == ":matching" then
     ripple_speed = "slow"
+  else
+    ripple_speed = "normal"
   end
   render_ripple()
 
@@ -290,8 +259,6 @@ function mission:render() -- override
     spr(117, 70, 119)
     print_outlined("raise gates", 81, 120, 7, 0)
   end
-
-  render_match_circles()
 
   if flr(t() * 2) % 2 == 0 then
     print_outlined("match", 84, 2, 0, 12)
