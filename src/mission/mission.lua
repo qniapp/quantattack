48b2708e40978c0e848ca9c9da9e6fb73c3e3909
Yasuhito Takamiya <yasuhito@gmail.com>
2022-12-03 11:12:28 +0900

    風船が上から入ってくる演出を追加

--- src/mission/mission.lua
+++ src/mission/mission.lua
@@ -193,6 +193,7 @@ function mission:update()
       for i, each in pairs(current_wave) do
         task_balloon:create(each, board.offset_x + board.width, i % 3 * 15, (i % 2) * 38)
       end
+      task_balloon:enter_all()
     else
       board.win = true
     end
@@ -223,9 +224,15 @@ function mission:render() -- override
     print_outlined("raise gates", 81, 120, 7, 0)
   end
 
-  if flr(t() * 2) % 2 == 0 then
-    print_outlined("match", 84, 2, 0, 12)
-    print_outlined("the pattern!", 70, 10, 0, 12)
+  if task_balloon.state == ":enter" then
+    if flr(t() * 4) % 2 == 0 then
+      print_outlined("wave #" .. wave_number, 80, 10, 0, 8)
+    end
+  else
+    if flr(t() * 2) % 2 == 0 then
+      print_outlined("match", 84, 2, 0, 12)
+      print_outlined("the pattern!", 70, 10, 0, 12)
+    end
   end
 end
 
