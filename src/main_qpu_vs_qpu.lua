0bbbf6c9d73f95613dfd99f2e71de4a71002bf71
Yasuhito Takamiya <yasuhito@gmail.com>
2022-12-16 06:29:43 +0900

    qpu_vs_qpu を新しい board クラスに対応

--- src/main_qpu_vs_qpu.lua
+++ src/main_qpu_vs_qpu.lua
@@ -1,13 +1,13 @@
-require("lib/board")
 require("lib/qpu")
 
 local game_class = require("lib/game")
 local game = game_class()
 
 local cursor_class = require("lib/cursor")
+local board_class = require("lib/board")
 
 local qpu1_cursor, qpu2_cursor = cursor_class(), cursor_class()
-local qpu1_board, qpu2_board = create_board(qpu1_cursor, 3), create_board(qpu2_cursor, 78)
+local qpu1_board, qpu2_board = board_class(qpu1_cursor, 3), board_class(qpu2_cursor, 78)
 qpu1_board.gate_offset_target, qpu2_board.gate_offset_target = { 3 + 24, 0 }, { 78 + 24, 0 }
 qpu1_board.attack_cube_target, qpu2_board.attack_cube_target = { 78 + 24, 0 }, { 3 + 24, 0 }
 local qpu1, qpu2 = create_qpu(qpu1_cursor, qpu1_board, 1), create_qpu(qpu2_cursor, qpu2_board, 1)
