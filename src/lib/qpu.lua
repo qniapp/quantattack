7c418485e49bf363013072233745a8cc2873425c
Yasuhito Takamiya <yasuhito@gmail.com>
2022-12-24 06:04:53 +0900

    トークン削減

--- src/lib/qpu.lua
+++ src/lib/qpu.lua
@@ -53,13 +53,10 @@ function create_qpu(board, _level)
           add(commands, "o")
           add_sleep_command(_ENV, 3)
         else
-          if not for_all_reducible_blocks(_ENV, _reduce_cnot) then
-            if not board.contains_garbage_match_block then
-              if not for_all_reducible_blocks(_ENV, _flatten_block) then
-                for_all_reducible_blocks(_ENV, _reduce_single_block)
-              end
-            end
-          end
+          return for_all_reducible_blocks(_ENV, _reduce_cnot) or
+              board.contains_garbage_match_block or
+              for_all_reducible_blocks(_ENV, _flatten_block) or
+              for_all_reducible_blocks(_ENV, _reduce_single_block)
         end
       end
     end,
