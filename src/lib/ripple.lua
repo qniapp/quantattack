90ee4fa8b6d30bab1142261c8154c4010640b524
Yasuhito Takamiya <yasuhito@gmail.com>
2023-01-14 14:35:49 +0900

    トークン削減

--- src/lib/ripple.lua
+++ src/lib/ripple.lua
@@ -1,3 +1,5 @@
+---@diagnostic disable: global-in-nil-env, lowercase-global
+
 local ripple_class = new_class()
 
 function ripple_class._init(_ENV)
@@ -13,12 +15,14 @@ end
 function ripple_class.render(_ENV)
   for i = -5, 5 do
     for j = -5, 5 do
-      local ang = atan2(i, j)
-      local d = sqrt(i * i + j * j)
+      local ang, d = atan2(i, j), sqrt(i * i + j * j)
       local r = 2 + 2 * sin(d / 4 + t2)
-      local h = 3 * r
-      local clr = (slow and r > 3 and tick % 2 == 0) and 13 or 1
-      circfill(64 + 12 * d * cos(ang + t1), 64 + 12 * d * sin(ang + t1) - h, r, clr)
+      circfill(
+        64 + 12 * d * cos(ang + t1),
+        64 + 12 * d * sin(ang + t1) - 3 * r,
+        r,
+        (slow and r > 3 and tick % 2 == 0) and 13 or 1
+      )
     end
   end
 end
