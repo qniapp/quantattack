9e5aee55f5e8862f29a775b989c2e900936c4535
Yasuhito Takamiya <yasuhito@gmail.com>
2023-01-25 08:48:45 +0900

    スクリプトを修正

--- run_game.sh
+++ run_game.sh
@@ -2,7 +2,7 @@
 
 # Run debug build with PICO-8 executable
 # Pass any extra arguments to pico8
-run_cmd="/Applications/PICO-8.app/Contents/MacOS/pico8 -run build/game.p8 -screenshot_scale 4 -gif_scale 4 $@"
+run_cmd="/Applications/PICO-8.app/Contents/MacOS/pico8 -run build/v0.5.0_debug/quantattack_title.p8 -screenshot_scale 4 -gif_scale 4 $@"
 
 echo "> $run_cmd"
 bash -c "$run_cmd"
