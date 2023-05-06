1d06a79c7d3147a7f56f2c986ca5f395d655c8df
Yasuhito Takamiya <yasuhito@gmail.com>
2023-05-06 10:18:09 +0900

    H のパターンに対応

--- scripts/generate_rule.rb
+++ scripts/generate_rule.rb
@@ -1,31 +1,80 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-score = nil
-gates = []
-reduce_to = []
-
-File.read(ARGV[0]).split("\n").each_with_index do |each, lineno|
-  if lineno.zero?
-    # 一行目は点数
-    score = each.to_i
-  else
-    case each
-    when /^│\sH/
-      gates << 'h'
-      reduce_to << ['h', 1 - gates.length]
-    end
-  end
+#
+# ファイルから読み込んだパターン文字列を空行で区切ったもの
+#
+def aa_patterns
+  File.read(ARGV[0]).split(/\n{2,}/)
 end
 
-gates_string = gates.join("\n")
-reduce_to_string = reduce_to.map do |each|
-  # dx, dy, block_type をカンマでつなげたものを返す。
-  dx = '' # 一列のパターンと仮定
-  dy = (each[1]).zero? ? '' : each[1]
-  block_type = '' # 消えて I になると仮定
+def parse_pattern(string)
+  score = nil
+  gates = []
+  reduce_to = []
 
-  "#{dx},#{dy},#{block_type}"
-end.join("\n")
+  string.split("\n").each_with_index do |each, lineno|
+    if lineno.zero?
+      # 一行目は点数
+      score = each.to_i
+    else
+      case each
+      when /^│\sH\s│.*([I|X|Y|Z])/ # パターン一列目の H
+        gates << 'h'
+        reduce_to << { dx: 0, dy: 1 - gates.length,
+                       block_type: Regexp.last_match(1) == 'I' ? '' : Regexp.last_match(1).downcase }
+      when /^\s+│\sH\s│.*([I|X|Y|Z])/ # パターン二列目の H
+        gates << '?,h'
+        reduce_to << { dx: true, dy: 1 - gates.length,
+                       block_type: Regexp.last_match(1) == 'I' ? '' : Regexp.last_match(1).downcase }
+      when /^│\sX\s│.*([I|X|Y|Z])/ # パターン一列目の X
+        gates << 'x'
+        # reduce_to << { dx: 0, dy: 1 - gates.length, block_type: '' }
+        reduce_to << { dx: 0, dy: 1 - gates.length,
+                       block_type: Regexp.last_match(1) == 'I' ? '' : Regexp.last_match(1).downcase }
+      when /^\s+│\sX\s│.*([I|X|Y|Z])/ # パターン二列目の X
+        gates << '?,x'
+        reduce_to << { dx: true, dy: 1 - gates.length,
+                       block_type: Regexp.last_match(1) == 'I' ? '' : Regexp.last_match(1).downcase }
+      when /^│\sY\s│.*([I|X|Y|Z])/ # パターン一列目の Y
+        gates << 'y'
+        reduce_to << { dx: 0, dy: 1 - gates.length,
+                       block_type: Regexp.last_match(1) == 'I' ? '' : Regexp.last_match(1).downcase }
+      when /^\s+│\sY\s│.*([I|X|Y|Z])/ # パターン二列目の Y
+        gates << '?,y'
+        reduce_to << { dx: 0, dy: 1 - gates.length,
+                       block_type: Regexp.last_match(1) == 'I' ? '' : Regexp.last_match(1).downcase }
+      when /^│\sZ\s│.*([I|X|Y|Z])/ # パターン一列目の Z
+        gates << 'z'
+        reduce_to << { dx: 0, dy: 1 - gates.length,
+                       block_type: Regexp.last_match(1) == 'I' ? '' : Regexp.last_match(1).downcase }
+      when /^\s+│\sZ\s│.*([I|X|Y|Z])/ # パターン二列目の Z
+        gates << '?,z'
+        reduce_to << { dx: 0, dy: 1 - gates.length,
+                       block_type: Regexp.last_match(1) == 'I' ? '' : Regexp.last_match(1).downcase }
+      when /^\s\sX─+X/
+        gates << 'swap,swap'
+      end
+    end
+  end
 
-p "#{gates_string}|#{reduce_to_string}|#{score}"
+  [score, gates, reduce_to]
+end
+
+aa_patterns.each do |pattern|
+  puts pattern
+
+  score, gates, reduce_to = parse_pattern(pattern)
+
+  gates_string = gates.join("\n")
+  reduce_to_string = reduce_to.map do |each|
+    dx = each[:dx] == true ? true : ''
+    dy = (each[:dy]).zero? ? '' : each[:dy]
+    block_type = each[:block_type]
+
+    "#{dx},#{dy},#{block_type}"
+  end.join("\n")
+
+  p "#{gates_string}|#{reduce_to_string}|#{score}"
+  puts
+end
