b7872b3b7d167db648fdbc2018fed73f4b367a07
Yasuhito Takamiya <yasuhito@gmail.com>
2023-05-18 07:49:42 +0900

    テスト修正

--- test/particle_utest.lua
+++ test/particle_utest.lua
@@ -1,4 +1,5 @@
 require("engine/test/bustedhelper")
+require("lib/helpers")
 require("lib/board")
 
 describe('particle', function()
@@ -10,30 +11,30 @@ describe('particle', function()
 
   describe('create', function()
     it("creates a particle", function()
-      particles:_create(1, 1, 1, 1, 7, 5, 1, 1, 1, 1, 10)
-      particles:_create(2, 2, 2, 2, 8, 11, 1, 1, 1, 1, 20)
+      particles:create({ 1, 1 }, "1,1,7,5,1,1,1,1,10")
+      particles:create({ 2, 2 }, "2,2,8,11,1,1,1,1,20")
 
       assert.are_equal(2, #particles.all)
 
       assert.are_equal(1, particles.all[1]._x)
       assert.are_equal(1, particles.all[1]._y)
-      assert.are_equal(1, particles.all[1]._radius)
-      assert.are_equal(colors["white"], particles.all[1]._color)
-      assert.are_equal(colors["dark_gray"], particles.all[1]._color_fade)
+      assert.are_equal(1, tonum(particles.all[1]._radius))
+      assert.are_equal(colors["white"], tonum(particles.all[1]._color))
+      assert.are_equal(colors["dark_gray"], tonum(particles.all[1]._color_fade))
       assert.are_equal(0, particles.all[1]._tick)
 
       assert.are_equal(2, particles.all[2]._x)
       assert.are_equal(2, particles.all[2]._y)
-      assert.are_equal(2, particles.all[2]._radius)
-      assert.are_equal(colors["red"], particles.all[2]._color)
-      assert.are_equal(colors["green"], particles.all[2]._color_fade)
+      assert.are_equal(2, tonum(particles.all[2]._radius))
+      assert.are_equal(colors["red"], tonum(particles.all[2]._color))
+      assert.are_equal(colors["green"], tonum(particles.all[2]._color_fade))
       assert.are_equal(0, particles.all[2]._tick)
     end)
   end)
 
   describe('update', function()
     it("updates all partciles", function()
-      particles:_create(1, 1, 1, 1, 7, 5, 1, 1, 1, 1, 20)
+      particles:create({ 1, 1 }, "1,1,7,5,1,1,1,1,20")
 
       assert.are_equal(1, #particles.all)
 
@@ -45,7 +46,7 @@ describe('particle', function()
 
   describe('render', function()
     it("renders all particles", function()
-      particles:_create(1, 1, 1, 1, 7, 5, 1, 1, 1, 1, 20)
+      particles:create({ 1, 1 }, "1,1,7,5,1,1,1,1,20")
 
       assert.has_no.errors(function()
         particles:render_all()
