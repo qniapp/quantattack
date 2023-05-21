99e8ce19eaa42c7718fd36529054aa5c08d5de1d
Yasuhito Takamiya <yasuhito@gmail.com>
2023-05-21 16:38:50 +0900

    テストの整理

---
+++ test/block/z_gate_utest.lua
@@ -0,0 +1,64 @@
+require("engine/test/bustedhelper")
+require("lib/block")
+
+describe('z_block #solo', function()
+  local z
+
+  before_each(function()
+    z = block_class("z")
+  end)
+
+  describe(".type", function()
+    it('should be "z"', function()
+      assert.are.equals("z", z.type)
+    end)
+  end)
+
+  describe(".state", function()
+    it('should be "idle"', function()
+      assert.is_true(z.state == "idle")
+    end)
+  end)
+
+  describe(".span", function ()
+    it("should be 1", function()
+      assert.are.equals(1, z.span)
+    end)
+  end)
+
+  describe(".height", function ()
+    it("should be 1", function()
+      assert.are.equals(1, z.height)
+    end)
+  end)
+
+  describe("stringify", function()
+    it("should return 'Z '", function()
+      assert.are.equals("Z ", stringify(z))
+    end)
+  end)
+
+  describe("is_fallable()", function()
+    it("should return true", function()
+      assert.is_true(z:is_fallable())
+    end)
+  end)
+
+  describe("is_reducible()", function()
+    it("should return true", function()
+      assert.is_true(z:is_reducible())
+    end)
+  end)
+
+  describe("is_empty", function()
+    it("should return false", function()
+      assert.is_false(z:is_empty())
+    end)
+  end)
+
+  describe("is_single_block", function()
+    it("should return true", function()
+      assert.is_true(z:is_single_block())
+    end)
+  end)
+end)
