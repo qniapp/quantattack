79922cd1e4eb6ddfc5987e6d8fcfd6606b47c07a
Yasuhito Takamiya <yasuhito@gmail.com>
2023-05-21 16:44:21 +0900

    テストを追加

---
+++ test/block/s_block_utest.lua
@@ -0,0 +1,64 @@
+require("engine/test/bustedhelper")
+require("lib/block")
+
+describe('s_block #solo', function()
+  local s
+
+  before_each(function()
+    s = block_class("s")
+  end)
+
+  describe(".type", function()
+    it('should be "s"', function()
+      assert.are.equals("s", s.type)
+    end)
+  end)
+
+  describe(".state", function()
+    it('should be "idle"', function()
+      assert.is_true(s.state == "idle")
+    end)
+  end)
+
+  describe(".span", function ()
+    it("should be 1", function()
+      assert.are.equals(1, s.span)
+    end)
+  end)
+
+  describe(".height", function ()
+    it("should be 1", function()
+      assert.are.equals(1, s.height)
+    end)
+  end)
+
+  describe("stringify", function()
+    it("should return 'S '", function()
+      assert.are.equals("S ", stringify(s))
+    end)
+  end)
+
+  describe("is_fallable()", function()
+    it("should return true", function()
+      assert.is_true(s:is_fallable())
+    end)
+  end)
+
+  describe("is_reducible()", function()
+    it("should return true", function()
+      assert.is_true(s:is_reducible())
+    end)
+  end)
+
+  describe("is_empty", function()
+    it("should return false", function()
+      assert.is_false(s:is_empty())
+    end)
+  end)
+
+  describe("is_single_block", function()
+    it("should return true", function()
+      assert.is_true(s:is_single_block())
+    end)
+  end)
+end)
