6ba4762813580c35ad3f09b8485557c25bf61964
Yasuhito Takamiya <yasuhito@gmail.com>
2023-05-23 04:35:39 +0900

    リファクタリング

--- test/block/garbage_block_utest.lua
+++ test/block/garbage_block_utest.lua
@@ -17,13 +17,11 @@ describe('garbage_block', function()
     end)
 
     it("幅を 2 以下に指定するとエラー", function()
-      ---@diagnostic disable-next-line: param-type-mismatch
-      assert.error(function() garbage_block(2) end)
+      assert.has.errors(function() garbage_block(2) end)
     end)
 
     it("幅を 7 以上に指定するとエラー", function()
-      ---@diagnostic disable-next-line: param-type-mismatch
-      assert.error(function() garbage_block(7) end)
+      assert.has.errors(function() garbage_block(7) end)
     end)
   end)
 
@@ -42,15 +40,33 @@ describe('garbage_block', function()
   end)
 
   describe('body_color', function()
-    it("色を指定して生成", function()
-      local garbage = garbage_block(6, 1, 2)
+    it("色を指定して生成 (2, 3, 4)", function()
+      local garbage
 
+      garbage = garbage_block(6, 1, 2)
       assert.are_equal(2, garbage.body_color)
+
+      garbage = garbage_block(6, 1, 3)
+      assert.are_equal(3, garbage.body_color)
+
+      garbage = garbage_block(6, 1, 4)
+      assert.are_equal(4, garbage.body_color)
     end)
 
     it("色が正しくない場合エラー", function()
-      ---@diagnostic disable-next-line: param-type-mismatch
-      assert.error(function() garbage_block(1) end)
+      assert.has.errors(function() garbage_block(3, 1, 0) end)
+      assert.has.errors(function() garbage_block(3, 1, 1) end)
+      assert.has.errors(function() garbage_block(3, 1, 5) end)
+      assert.has.errors(function() garbage_block(3, 1, 6) end)
+      assert.has.errors(function() garbage_block(3, 1, 7) end)
+      assert.has.errors(function() garbage_block(3, 1, 8) end)
+      assert.has.errors(function() garbage_block(3, 1, 9) end)
+      assert.has.errors(function() garbage_block(3, 1, 10) end)
+      assert.has.errors(function() garbage_block(3, 1, 11) end)
+      assert.has.errors(function() garbage_block(3, 1, 12) end)
+      assert.has.errors(function() garbage_block(3, 1, 13) end)
+      assert.has.errors(function() garbage_block(3, 1, 14) end)
+      assert.has.errors(function() garbage_block(3, 1, 15) end)
     end)
 
     it("色に応じて inner_border_color をセットする", function()
@@ -60,6 +76,57 @@ describe('garbage_block', function()
     end)
   end)
 
+  it("指定した幅と高さで board 上の位置を占有する", function()
+    local board = board_class()
+
+      -- _ _ _ _ _ _
+      -- _ ■ ■ ■ _ _
+      -- _ ■ ■ ■ _ _
+      -- _ ■ ■ ■ _ _
+      -- _ _ _ _ _ _
+      board:put(2, 2, garbage_block(3, 3))
+
+      -- 5 行目は空
+      assert.is_true(board:is_empty(1, 5))
+      assert.is_true(board:is_empty(2, 5))
+      assert.is_true(board:is_empty(3, 5))
+      assert.is_true(board:is_empty(4, 5))
+      assert.is_true(board:is_empty(5, 5))
+      assert.is_true(board:is_empty(6, 5))
+
+      -- 4 行目は x = 1, 5, 6 のみ空
+      assert.is_true(board:is_empty(1, 4))
+      assert.is_false(board:is_empty(2, 4))
+      assert.is_false(board:is_empty(3, 4))
+      assert.is_false(board:is_empty(4, 4))
+      assert.is_true(board:is_empty(5, 4))
+      assert.is_true(board:is_empty(6, 4))
+
+      -- 3 行目も x = 1, 5, 6 のみ空
+      assert.is_true(board:is_empty(1, 3))
+      assert.is_false(board:is_empty(2, 3))
+      assert.is_false(board:is_empty(3, 3))
+      assert.is_false(board:is_empty(4, 3))
+      assert.is_true(board:is_empty(5, 3))
+      assert.is_true(board:is_empty(6, 3))
+
+      -- 2 行目も x = 1, 5, 6 のみ空
+      assert.is_true(board:is_empty(1, 2))
+      assert.is_false(board:is_empty(2, 2))
+      assert.is_false(board:is_empty(3, 2))
+      assert.is_false(board:is_empty(4, 2))
+      assert.is_true(board:is_empty(5, 2))
+      assert.is_true(board:is_empty(6, 2))
+
+      -- 1 行目は空
+      assert.is_true(board:is_empty(1, 1))
+      assert.is_true(board:is_empty(2, 1))
+      assert.is_true(board:is_empty(3, 1))
+      assert.is_true(board:is_empty(4, 1))
+      assert.is_true(board:is_empty(5, 1))
+      assert.is_true(board:is_empty(6, 1))
+  end)
+
   describe('render', function()
     it('おじゃまブロックの本体、影、内側の枠線を描画', function()
       local garbage = garbage_block(6, 1, 2)
@@ -67,66 +134,7 @@ describe('garbage_block', function()
 
       garbage:render(50, 50)
 
-      ---@diagnostic disable-next-line: undefined-field
-      assert.spy(garbage._render_box).was_called(3)
-      ---@diagnostic disable-next-line: undefined-field
-      assert.spy(garbage._render_box).was_called_with(51, 51, 95, 55, 14)
-    end)
-  end)
-end)
-
-describe("board", function()
-  describe("_garbage_head_block", function()
-    it("指定した座標がおじゃまブロックに含まれるかどうかを判定", function()
-      local board = board_class()
-
-      -- 行
-      -- _ _ _ _ _ _
-      -- _ g g g _ _
-      -- _ g g g _ _
-      -- _ g g g _ _
-      -- _ _ _ _ _ _
-      board:put(2, 2, garbage_block(3, 3))
-
-      -- 5 行目
-      assert.is_false(board:_garbage_head_block(1, 5) ~= nil)
-      assert.is_false(board:_garbage_head_block(2, 5) ~= nil)
-      assert.is_false(board:_garbage_head_block(3, 5) ~= nil)
-      assert.is_false(board:_garbage_head_block(4, 5) ~= nil)
-      assert.is_false(board:_garbage_head_block(5, 5) ~= nil)
-      assert.is_false(board:_garbage_head_block(6, 5) ~= nil)
-
-      -- 4 行目
-      assert.is_false(board:_garbage_head_block(1, 4) ~= nil)
-      assert.is_true(board:_garbage_head_block(2, 4) ~= nil)
-      assert.is_true(board:_garbage_head_block(3, 4) ~= nil)
-      assert.is_true(board:_garbage_head_block(4, 4) ~= nil)
-      assert.is_false(board:_garbage_head_block(5, 4) ~= nil)
-      assert.is_false(board:_garbage_head_block(6, 4) ~= nil)
-
-      -- 3 行目
-      assert.is_false(board:_garbage_head_block(1, 3) ~= nil)
-      assert.is_true(board:_garbage_head_block(2, 3) ~= nil)
-      assert.is_true(board:_garbage_head_block(3, 3) ~= nil)
-      assert.is_true(board:_garbage_head_block(4, 3) ~= nil)
-      assert.is_false(board:_garbage_head_block(5, 3) ~= nil)
-      assert.is_false(board:_garbage_head_block(6, 3) ~= nil)
-
-      -- 2 行目
-      assert.is_false(board:_garbage_head_block(1, 2) ~= nil)
-      assert.is_true(board:_garbage_head_block(2, 2) ~= nil)
-      assert.is_true(board:_garbage_head_block(3, 2) ~= nil)
-      assert.is_true(board:_garbage_head_block(4, 2) ~= nil)
-      assert.is_false(board:_garbage_head_block(5, 2) ~= nil)
-      assert.is_false(board:_garbage_head_block(6, 2) ~= nil)
-
-      -- 1 行目
-      assert.is_false(board:_garbage_head_block(1, 1) ~= nil)
-      assert.is_false(board:_garbage_head_block(2, 1) ~= nil)
-      assert.is_false(board:_garbage_head_block(3, 1) ~= nil)
-      assert.is_false(board:_garbage_head_block(4, 1) ~= nil)
-      assert.is_false(board:_garbage_head_block(5, 1) ~= nil)
-      assert.is_false(board:_garbage_head_block(6, 1) ~= nil)
+      assert.spy(garbage._render_box).was.called(3)
     end)
   end)
 end)
